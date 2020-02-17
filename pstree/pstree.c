6666bce99a8236bddb8650b6c4b207f6255f2257
tracer-nju <tracer@nju.edu.cn>
2020-02-17 22:42:30 +0800

    Static hostname: lpr-gram          Icon name: computer-laptop            Chassis: laptop         Machine ID: 933366a46148405d9dd39617f253434f            Boot ID: 392503b32531448e98b94c5a10259c0f   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 5.3.0-28-generic       Architecture: x86-64  22:42:30 up  2:15,  1 user,  load average: 1.73, 0.86, 0.49

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -1,11 +1,34 @@
 #include <stdio.h>
 #include <assert.h>
+#include <string.h>
+#include <stdbool.h>
 
 int main(int argc, char *argv[]) {
-  for (int i = 0; i < argc; i++) {
-    assert(argv[i]);
-    printf("argv[%d] = %s\n", i, argv[i]);
-  }
-  assert(!argv[argc]);
-  return 0;
+    typedef bool pstree_option;
+    pstree_option pstree_show_pids, pstree_numeric_sort, pstree_version;
+    pstree_show_pids = pstree_numeric_sort = pstree_version = false;
+    for (int i = 0; i < argc; i++) {
+        assert(argv[i]);
+        printf("argv[%d] = %s\n", i, argv[i]);
+        if (strcmp(argv[i], "-p") == 0 || strcmp(argv[i], "--show-pids") == 0 || argc == 1)
+            pstree_show_pids = true;
+        if (strcmp(argv[i], "-n") == 0 || strcmp(argv[i], "numeric-sort") == 0)
+            pstree_numeric_sort = true;
+        if (strcmp(argv[i], "-V") == 0 || strcmp(argv[i], "--version") == 0)
+            pstree_version = true;
+    }
+
+
+    if(pstree_version == true) {
+        printf("pstree (PSmisc) 23.1\nCopyright (C) 1993-2017 Werner Almesberger and Craig Small\n\nPSmisc 不提供任何保证。\n该程序为自由软件，欢迎你在 GNU 通用公共许可证 (GPL) 下重新发布。\n详情可参阅 COPYING 文件。\n");
+        return 0;
+    }
+    if (pstree_show_pids == true) {
+        printf("show-pids\n");
+    }
+    if (pstree_numeric_sort == true) {
+        printf("numeric-sort\n");
+    }
+    assert(!argv[argc]);
+    return 0;
 }
