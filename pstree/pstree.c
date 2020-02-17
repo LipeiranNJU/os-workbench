0b49beba3560412465b2ef6a7742dada0f9b15ac
tracer-nju <tracer@nju.edu.cn>
2020-02-17 23:13:40 +0800

    Static hostname: lpr-gram          Icon name: computer-laptop            Chassis: laptop         Machine ID: 933366a46148405d9dd39617f253434f            Boot ID: 392503b32531448e98b94c5a10259c0f   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 5.3.0-28-generic       Architecture: x86-64  23:13:40 up  2:46,  1 user,  load average: 0.96, 0.73, 0.54

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -2,6 +2,7 @@
 #include <assert.h>
 #include <string.h>
 #include <stdbool.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[]) {
     typedef bool pstree_option;
@@ -18,8 +19,9 @@ int main(int argc, char *argv[]) {
             pstree_version = true;
     }
 
-
-    if(pstree_version == true) {
+    pid_t pid = getpid();
+    printf("Pid:%D\n",pid);
+    if (pstree_version == true) {
         printf("pstree (PSmisc) 23.1\nCopyright (C) 1993-2017 Werner Almesberger and Craig Small\n\nPSmisc 不提供任何保证。\n该程序为自由软件，欢迎你在 GNU 通用公共许可证 (GPL) 下重新发布。\n详情可参阅 COPYING 文件。\n");
         return 0;
     }
