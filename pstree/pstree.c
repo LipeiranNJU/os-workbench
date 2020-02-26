25f0d28717e5d61f7a76c479da840cabf7593d2f
tracer-nju <tracer@nju.edu.cn>
2020-02-26 20:20:37 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 92ae99bcd1cf469483513ada2039dcad            Boot ID: 62b4f0a961a04b8f955e0fcad7978b97     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-8-amd64       Architecture: x86-64  20:20:37 up  4:58,  2 users,  load average: 0.45, 0.22, 0.16

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -75,7 +75,7 @@ int main(int argc, char *argv[]) {
         fgets(one_line, 100, fp);//ppid
         // printf("%s\n", one_line + 6);
         strtok(one_line, "\t");
-        strtok(NULL, "\t");
+        // strtok(NULL, "\t");
         printf("ppid:%s", strtok(NULL, "\t"));
 
     }
