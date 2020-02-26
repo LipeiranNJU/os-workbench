845f77106843783c5b926b13873be17b32da2209
tracer-nju <tracer@nju.edu.cn>
2020-02-26 20:14:31 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 92ae99bcd1cf469483513ada2039dcad            Boot ID: 62b4f0a961a04b8f955e0fcad7978b97     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-8-amd64       Architecture: x86-64  20:14:31 up  4:51,  2 users,  load average: 0.07, 0.12, 0.14

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -65,7 +65,7 @@ int main(int argc, char *argv[]) {
         // memset(current_path, '\0', 100);
         FILE* fp = fopen(current_path, "r");
         fgets(one_line, 100, fp);
-        printf("Head line:\n%s", one_line + 5);
+        printf("Head line:\n%s", one_line + 6);
         fgets(one_line, 100, fp);
         fgets(one_line, 100, fp);
         fgets(one_line, 100, fp);
