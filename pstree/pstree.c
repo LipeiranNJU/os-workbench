6cea355d46ee899179087bcdd311ee90124e6cfb
tracer-nju <tracer@nju.edu.cn>
2020-02-26 15:29:45 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 92ae99bcd1cf469483513ada2039dcad            Boot ID: 62b4f0a961a04b8f955e0fcad7978b97     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-8-amd64       Architecture: x86-64  15:29:46 up  2:08,  2 users,  load average: 0.00, 0.02, 0.00

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -5,6 +5,16 @@
 #include <unistd.h>
 #include <dirent.h>
 
+void showdir(char* path_of_dir) {
+	DIR dir;
+	struct dirent *ptr;
+	dir = opendir(path_of_dir) {
+		while((ptr = read_dir(dir)) != NULL) {
+			print("%s is in the directory %s.\n", ptr->d_name, path_of_dir);
+		}
+	}
+}
+
 int main(int argc, char *argv[]) {
     typedef bool pstree_option;
     pstree_option pstree_show_pids, pstree_numeric_sort, pstree_version;
@@ -17,7 +27,7 @@ int main(int argc, char *argv[]) {
         if (strcmp(argv[i], "-n") == 0 || strcmp(argv[i], "numeric-sort") == 0)
             pstree_numeric_sort = true;
         if (strcmp(argv[i], "-V") == 0 || strcmp(argv[i], "--version") == 0)
-            pstree_version = true;
+            pstree_version = true; 
     }
     pid_t pid = getpid();
     printf("Pid:%d\n",pid);
@@ -31,17 +41,17 @@ int main(int argc, char *argv[]) {
     while((ptr = readdir(dir)) != NULL) {
 	    if (ptr->d_name[0]>='0' && ptr->d_name[0] <= '9') {
 	    	printf("dir name:%s\n", ptr->d_name);
-		char process_dir[100];
-		strcat(strcat(strcpy(process_dir, initial_path),"/"),(ptr->d_name));
-		printf("dir path:%s\n",process_dir);
-		DIR *dir2;
-		dir2 = opendir(process_dir);
-		while ((ptr2 = readdir(dir2)) != NULL) {
-			printf("process name:%s\n", ptr2->d_name);
-		}
+			char process_dir[100];
+			strcat(strcat(strcpy(process_dir, initial_path),"/"),(ptr->d_name));
+			printf("dir path:%s\n",process_dir);
+		//	DIR *dir2;
+		//	dir2 = opendir(process_dir);
+		//	while ((ptr2 = readdir(dir2)) != NULL) {
+		//		printf("process name:%s\n", ptr2->d_name);
 
-		break;
-	    }
+		//	}
+			break;
+		}
     }
     closedir(dir);
     
