edf7e9ad23fe8d60fed3e68d346ed3fa785e6937
tracer-nju <tracer@nju.edu.cn>
2020-02-27 15:22:44 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 92ae99bcd1cf469483513ada2039dcad            Boot ID: 95e45e96e4e74a2d96429d34bdf5fb0e     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-8-amd64       Architecture: x86-64  15:22:44 up  2:43,  2 users,  load average: 0.52, 0.22, 0.08

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -124,7 +124,7 @@ int main(int argc, char *argv[]) {
         // if (strncmp(list[i].name, "bash", 4) == 0)
         //     printf("list[%d].name is %slist[i].pid is %d\nlist[i].ppid is %d\n\n", i, list[i].name, list[i].pid, list[i].ppid);
         if (strncmp(list[i].name, "bash", strlen("bash")) == 0 && list[i].pid ==1319) {
-            printf("%s\nAWESOME!!!!\ni:%dpid:%d ppid:%d\n\n", list[i].name, i, list[i].pid, list[i].ppid);
+            printf("%s\nAWESOME!!!!i:%dpid:%d ppid:%d\n\n", list[i].name, i, list[i].pid, list[i].ppid);
             // assert(0);
         }
 
