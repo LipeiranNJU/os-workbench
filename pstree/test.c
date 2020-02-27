7d0308ee526b3a948beaf7509ccb84a401ac62bd
tracer-nju <tracer@nju.edu.cn>
2020-02-27 19:43:16 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 92ae99bcd1cf469483513ada2039dcad            Boot ID: 95e45e96e4e74a2d96429d34bdf5fb0e     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-8-amd64       Architecture: x86-64  19:43:17 up  7:04,  2 users,  load average: 0.81, 0.30, 0.20

---
+++ pstree/test.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main() {
+    printf("Hello gf ggggggggggggggggggggggggggggggffffffffffffffffffffffffffddfffffffffffffffffffffffggggggggggggggrrrrrrrrrr\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nggggggggggggggggggggggggggggggggggggggggWorld!\n");
+    fprintf(stderr, "test\n");
+    return 0;
+}
