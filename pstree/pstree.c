db0fa28fde23309bdf219e209e55b5162244f844
tracer-nju <tracer@nju.edu.cn>
2020-02-27 13:14:54 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 92ae99bcd1cf469483513ada2039dcad            Boot ID: 95e45e96e4e74a2d96429d34bdf5fb0e     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-8-amd64       Architecture: x86-64  13:14:54 up 35 min,  2 users,  load average: 0.38, 0.12, 0.08

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -135,7 +135,7 @@ int main(int argc, char *argv[]) {
         struct data tmp;
         for (int i = 0; i < nn - 1; i++) {
             for (int j = 0; j < nn - 1 - i; j++) {
-                if (strcmp(list[j].name, list[j + 1].name)) {
+                if (list[j].layer > list[j + 1].layer || (list[j].layer == list[j+1].layer && list[j].pid > list[j+1].pid)) {
                     strcpy(tmp.name, list[i].name);
                     tmp.pid = list[j].pid;
                     tmp.ppid = list[j].ppid;
