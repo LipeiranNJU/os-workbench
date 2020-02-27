9cebc3b94b3749ac7085fc3f21611182cc5174ba
tracer-nju <tracer@nju.edu.cn>
2020-02-27 15:24:56 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: 92ae99bcd1cf469483513ada2039dcad            Boot ID: 95e45e96e4e74a2d96429d34bdf5fb0e     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-8-amd64       Architecture: x86-64  15:24:56 up  2:45,  2 users,  load average: 0.47, 0.33, 0.14

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -118,13 +118,13 @@ int main(int argc, char *argv[]) {
         list[i].ppid = atoi(one_line + 6);
         list[i].layer = -1;
         if (strncmp(list[i].name, "xfce4-terminal", strlen("xfce4-terminal")) == 0 && list[i].pid ==1319) {
-            printf("$%sAWESOME!!!!\ni:%dpid:%d ppid:%d\n\n&", list[i].name, i, list[i].pid, list[i].ppid);
+            printf("\n\n$%sAWESOME!!!!\ni:%dpid:%d ppid:%d&\n\n", list[i].name, i, list[i].pid, list[i].ppid);
             // assert(0);
         }
         // if (strncmp(list[i].name, "bash", 4) == 0)
         //     printf("list[%d].name is %slist[i].pid is %d\nlist[i].ppid is %d\n\n", i, list[i].name, list[i].pid, list[i].ppid);
         if (strncmp(list[i].name, "bash", strlen("bash")) == 0 && list[i].pid ==1319) {
-            printf("@%sAWESOME!!!!i:%dpid:%d ppid:%d\n\n#", list[i].name, i, list[i].pid, list[i].ppid);
+            printf("\n\n@%sAWESOME!!!!i:%dpid:%d ppid:%d#\n\n", list[i].name, i, list[i].pid, list[i].ppid);
             // assert(0);
         }
 
