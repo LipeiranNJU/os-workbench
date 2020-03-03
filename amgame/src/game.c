88818cd1f29be835d3705625df1ff550025c3690
tracer-nju <tracer@nju.edu.cn>
2020-03-03 10:50:07 +0800

    Static hostname: promote          Icon name: computer-vm            Chassis: vm         Machine ID: 3094b99fa28744cba649dfd468648e55            Boot ID: b8dc7e10059f428b909f11ff39ff7ad4     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-8-amd64       Architecture: x86-64  10:50:08 up 42 min,  1 user,  load average: 0.01, 0.08, 0.05

--- amgame/src/game.c
+++ amgame/src/game.c
@@ -1,5 +1,8 @@
 #include <game.h>
 
+void _halt(int code);
+void read_key();
+
 // Operating system is a C program!
 int main(const char *args) {
   _ioe_init();
@@ -13,6 +16,16 @@ int main(const char *args) {
   puts("Press any key to see its key code...\n");
   while (1) {
     print_key();
+    read_key();
   }
   return 0;
+}
+
+
+void read_key() {
+  _DEV_INPUT_KBD_t event = { .keycode = _KEY_NONE };
+  _io_read(_DEV_INPUT, _DEVREG_INPUT_KBD, &event, sizeof(event));
+  if (event.keycode == _KEY_ESCAPE && event.keydown) {
+    _halt(0);
+  }
 }
