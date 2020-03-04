7ba66cbe5b2b1273dced0b99778acb119a562ba4
tracer-nju <tracer@nju.edu.cn>
2020-03-04 22:21:31 +0800

    Static hostname: promote          Icon name: computer-vm            Chassis: vm         Machine ID: 3094b99fa28744cba649dfd468648e55            Boot ID: 57ada00d93894bbb81ecef338969a2b4     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-8-amd64       Architecture: x86-64  22:21:31 up  4:23,  1 user,  load average: 0.38, 0.37, 0.45

--- amgame/src/game.c
+++ amgame/src/game.c
@@ -252,7 +252,7 @@ int read_key_of_mine(int game[][4]) {
   for (int i = 0; i < 4; i++) {
     for (int j = 0; j < 4; j++){
       if (game[i][j] >= 0)
-        _halt(12);
+        _halt(15);
     }
   }
 
