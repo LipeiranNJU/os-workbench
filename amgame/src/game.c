6e695d10657dadfeecc9588864957e2d13b617e8
tracer-nju <tracer@nju.edu.cn>
2020-03-04 23:22:26 +0800

    Static hostname: promote          Icon name: computer-vm            Chassis: vm         Machine ID: 3094b99fa28744cba649dfd468648e55            Boot ID: 57ada00d93894bbb81ecef338969a2b4     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-8-amd64       Architecture: x86-64  23:22:26 up  5:24,  1 user,  load average: 0.44, 0.57, 0.52

--- amgame/src/game.c
+++ amgame/src/game.c
@@ -298,7 +298,9 @@ void update_screen(int update, int** game) {
   for (int x = 0; x * SIDE <= w; x ++) {
     for (int y = 0; y * SIDE <= h; y++) {
       if (x <= 3 && y <= 3) {
-        draw_tile(x * SIDE, y * SIDE, SIDE, SIDE, block[game[x][y]+3]);
+        if ((game[0][0] + 3) < 0)
+        _halt(99);
+        draw_tile(x * SIDE, y * SIDE, SIDE, SIDE, block[0]);
       }
       else {
         // if ((x & 1) ^ (y & 1) ) {
