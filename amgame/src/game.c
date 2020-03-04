63a75d34c50624c50e80bf3aca33542bd52f6a84
tracer-nju <tracer@nju.edu.cn>
2020-03-04 23:26:45 +0800

    Static hostname: promote          Icon name: computer-vm            Chassis: vm         Machine ID: 3094b99fa28744cba649dfd468648e55            Boot ID: 57ada00d93894bbb81ecef338969a2b4     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-8-amd64       Architecture: x86-64  23:26:45 up  5:28,  1 user,  load average: 0.58, 0.69, 0.60

--- amgame/src/game.c
+++ amgame/src/game.c
@@ -267,19 +267,19 @@ int read_key_of_mine(int game[][4]) {
     puts(key_names[event.keycode]);
     puts("\n");
   }
-  if (event.keycode == _KEY_W && event.keydown) {
+  if (event.keycode == _KEY_A && event.keydown) {
     move_up(game, 4);
     return 1;
   }
-  if (event.keycode == _KEY_S && event.keydown) {
+  if (event.keycode == _KEY_D && event.keydown) {
     move_down(game, 4);
     return 1;
   }
-  if (event.keycode == _KEY_A && event.keydown) {
+  if (event.keycode == _KEY_W && event.keydown) {
     move_left(game, 4);
     return 1;
   }
-  if (event.keycode == _KEY_D && event.keydown) {
+  if (event.keycode == _KEY_S && event.keydown) {
     move_right(game, 4);
     return 1;
   }
