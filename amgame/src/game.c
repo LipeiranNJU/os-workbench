76a98ecd64b01968078d0d858e1ce43e34b221a9
tracer-nju <tracer@nju.edu.cn>
2020-03-05 10:01:54 +0800

    Static hostname: promote          Icon name: computer-vm            Chassis: vm         Machine ID: 3094b99fa28744cba649dfd468648e55            Boot ID: 57ada00d93894bbb81ecef338969a2b4     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-8-amd64       Architecture: x86-64  10:01:54 up  6:30,  1 user,  load average: 0.54, 0.47, 0.33

--- amgame/src/game.c
+++ amgame/src/game.c
@@ -255,11 +255,11 @@ int main(const char *args) {
     update_screen(mode, game);
     my_srand(153247);
     int tx,ty;
-    L:
+    // L:
     tx = my_rand()%4;
     ty = my_rand()%4;
-    if (game[tx][ty] >= 0)
-      goto L;
+    // if (game[tx][ty] >= 0)
+    //   goto L;
     game[tx][ty] = 0;
   }
   return 0;
