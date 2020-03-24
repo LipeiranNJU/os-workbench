de9500c6befcf555a185892901057ac3917df1e8
tracer-nju <tracer@nju.edu.cn>
2020-03-24 22:28:18 +0800

    Static hostname: lpr-PC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 4903748c3d604ed7ab52acdb5ca8aff2            Boot ID: ee211e7e174e4f23b7ffeb0cf4fe401b   Operating System: Deepin 15             Kernel: Linux 4.15.0-30deepin-generic       Architecture: x86-64  22:28:18 up  3:19,  1 user,  load average: 0.95, 0.77, 0.68

--- libco/co.c
+++ libco/co.c
@@ -1,14 +1,38 @@
 #include "co.h"
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+enum co_status {
+  CO_NEW = 1, // 新创建，还未执行过
+  CO_RUNNING, // 已经执行过
+  CO_WAITING, // 在 co_wait 上等待
+  CO_DEAD,    // 已经结束，但还未释放资源
+};
+
+struct co {
+  char *name;
+  void (*func)(void *); // co_start 指定的入口地址和参数
+  void *arg;
+
+  enum co_status status;  // 协程的状态
+  struct co *    waiter;  // 是否有其他协程在等待当前协程
+  jmp_buf        context; // 寄存器现场 (setjmp.h)
+  uint8_t        stack[STACK_SIZE]; // 协程的堆栈
+};
 
 struct co {
 };
 
 struct co *co_start(const char *name, void (*func)(void *), void *arg) {
+  static struct co co0;
+  strcpy(co0.name, name);
+  printf("HHH");
   return NULL;
 }
 
 void co_wait(struct co *co) {
+
 }
 
 void co_yield() {
