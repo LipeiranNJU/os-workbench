76ef5aded74bb002509422758bd010b343915088
tracer-nju <tracer@nju.edu.cn>
2020-03-25 01:07:32 +0800

    Static hostname: lpr-PC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 4903748c3d604ed7ab52acdb5ca8aff2            Boot ID: ee211e7e174e4f23b7ffeb0cf4fe401b   Operating System: Deepin 15             Kernel: Linux 4.15.0-30deepin-generic       Architecture: x86-64  01:07:33 up  5:59,  1 user,  load average: 0.70, 0.52, 0.43

--- libco/co.c
+++ libco/co.c
@@ -79,10 +79,11 @@ void co_wait(struct co *co) {
 }
 
 void co_yield() {
-  printf("TTT");
+  printf("TTT\n");
   // 显然使用stack_switch_call 来切换栈的，估计是把co最高地址/最高地址+1当做sp，func作为entry，arg作为参数
   // save data
   int val = setjmp(current->context);
+  printf("UUU\n");
   if (val == 0) {
     co* tmp = current;
     while (tmp->waiter != NULL) {
