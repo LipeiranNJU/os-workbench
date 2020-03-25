1e8a808013d5a32274d50efc8ebd301561256a51
tracer-nju <tracer@nju.edu.cn>
2020-03-25 19:52:52 +0800

    Static hostname: lpr-PC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 4903748c3d604ed7ab52acdb5ca8aff2            Boot ID: bc8d72d1c3144a5eb05c8ae3dd438945   Operating System: Deepin 15             Kernel: Linux 4.15.0-30deepin-generic       Architecture: x86-64  19:52:52 up  9:50,  1 user,  load average: 1.96, 1.09, 0.62

--- libco/co.c
+++ libco/co.c
@@ -6,10 +6,12 @@
 #include <stdint.h>
 #include <assert.h>
 int times = 0;
+void* sp1;
+uintptr_t arg1;
 static inline void stack_switch_call(void *sp, void *entry, uintptr_t arg) {
-  void* sp1 = sp;
-  uintptr_t arg1 = arg;
-    printf("sp1 in is:%lld\n arg in stack switch:%s\n", (unsigned long long)((uintptr_t)(sp1)) ,(char *) arg1);
+  sp1 = sp;
+  arg1 = arg;
+  printf("sp1 in is:%lld\n arg in stack switch:%s\n", (unsigned long long)((uintptr_t)(sp1)) ,(char *) arg1);
   asm volatile (
 #if __x86_64__
     "movq %0, %%rsp; movq %2, %%rdi; jmp *%1" : : "b"((uintptr_t)sp),     "d"(entry), "a"(arg)
