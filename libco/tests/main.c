bb473fa9977d95024d8fc7f808680acbfc265cec
tracer-nju <tracer@nju.edu.cn>
2020-03-25 20:55:50 +0800

    Static hostname: lpr-PC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 4903748c3d604ed7ab52acdb5ca8aff2            Boot ID: bc8d72d1c3144a5eb05c8ae3dd438945   Operating System: Deepin 15             Kernel: Linux 4.15.0-30deepin-generic       Architecture: x86-64  20:55:50 up 10:53,  1 user,  load average: 0.60, 0.40, 0.36

--- libco/tests/main.c
+++ libco/tests/main.c
@@ -42,7 +42,7 @@ static void work(void *arg) {
 }
 
 static void test_1() {
-
+    assert(0);
     struct co *thd1 = co_start("thread-1", work, "X");
     struct co *thd2 = co_start("thread-2", work, "Y");
 
