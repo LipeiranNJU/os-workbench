70b7df3a132f4a1cd33dda93f88d747ba937de78
tracer-nju <tracer@nju.edu.cn>
2020-03-25 13:52:55 +0800

    Static hostname: lpr-PC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 4903748c3d604ed7ab52acdb5ca8aff2            Boot ID: bc8d72d1c3144a5eb05c8ae3dd438945   Operating System: Deepin 15             Kernel: Linux 4.15.0-30deepin-generic       Architecture: x86-64  13:52:55 up  3:50,  1 user,  load average: 1.31, 1.32, 1.45

--- libco/co.c
+++ libco/co.c
@@ -92,7 +92,7 @@ void co_yield() {
   // 显然使用stack_switch_call 来切换栈的，估计是把co最高地址/最高地址+1当做sp，func作为entry，arg作为参数
   // save data
   int val = setjmp(current->context);
-  assert(current->waiter != NULL);
+  // assert(current->waiter != NULL);
   printf("after setjmp current addr:%llx\tcurrent->waiter:%llx\n", (unsigned long long) (uintptr_t) (current), (unsigned long long) (uintptr_t) (current->waiter));
   assert(current->waiter != NULL);
   printf("UUU\n");
