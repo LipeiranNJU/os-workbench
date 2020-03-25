9142b82478f9491129342bda6bd3cb50ddc61ab4
tracer-nju <tracer@nju.edu.cn>
2020-03-25 17:08:56 +0800

    Static hostname: lpr-PC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 4903748c3d604ed7ab52acdb5ca8aff2            Boot ID: bc8d72d1c3144a5eb05c8ae3dd438945   Operating System: Deepin 15             Kernel: Linux 4.15.0-30deepin-generic       Architecture: x86-64  17:08:56 up  7:06,  1 user,  load average: 0.94, 0.54, 0.42

--- libco/co.c
+++ libco/co.c
@@ -80,7 +80,8 @@ void co_wait(struct co *co) {
   printf("HHH\n");
   // uint8_t s[10000];
   if (co->status == CO_NEW) {
-    stack_switch_call(co+1, co->func, (uintptr_t)co->arg);
+    printf("new stack:%llx", (unsigned long long) ((uintptr_t)(co+1)));
+    stack_switch_call(co + 1, co->func, (uintptr_t)co->arg);
     printf("FUCK");
   }
 
