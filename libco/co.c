f72e0fa5c094a6b7783a2ce5c965fd030f8ca973
tracer-nju <tracer@nju.edu.cn>
2020-03-26 23:14:27 +0800

    Static hostname: lpr-13Z990-V-AA33C          Icon name: computer-laptop            Chassis: laptop         Machine ID: 304329bc72c74a7da6d2365956d3c081            Boot ID: 3fa3ca5911ff401c801d8290a8f403c6   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 5.3.0-43-generic       Architecture: x86-64  23:14:27 up  6:20,  1 user,  load average: 1.23, 1.16, 1.08

--- libco/co.c
+++ libco/co.c
@@ -11,7 +11,8 @@ uintptr_t arg1;
 static inline void stack_switch_call(void *sp, void *entry, uintptr_t arg) {
   sp1 = sp;
   arg1 = arg;
-  printf("sp1 in is:%llx\n arg in stack switch:%llx\n", (unsigned long long)((uintptr_t)(sp1)), (unsigned long long) arg);
+
+//   printf("sp1 in is:%llx\n arg in stack switch:%llx\n", (unsigned long long)((uintptr_t)(sp1)), (unsigned long long) arg);
   asm volatile (
 #if __x86_64__
     "movq %0, %%rsp; movq %2, %%rdi; jmp *%1" : : "b"((uintptr_t)sp),     "d"(entry), "a"(arg)
@@ -36,12 +37,12 @@ typedef enum co_status {
 typedef struct co {
   char *name;
   void (*func)(void *); // co_start 指定的入口地址和参数
-  void *arg;
+  char *arg;
 
   co_status status;  // 协程的状态
   struct co *    waiter;  // 是否有其他协程在等待当前协程
   jmp_buf        context; // 寄存器现场 (setjmp.h)
-  uint8_t        stack[STACK_SIZE]; // 协程的堆栈
+  uint8_t*        stack; // 协程的堆栈
 } co;
 
 co *current = NULL;
@@ -60,26 +61,21 @@ struct co *co_start(const char *name, void (*func)(void *), void *arg) {
     }
     tmp->waiter = pco;
   }
-  if (current->waiter != NULL) {
-    printf("Thread waiter:%llx\n",((unsigned long long)((uintptr_t)current->waiter)));
-  }
   pco->name = malloc(strlen(name)+1); // 分配协程名字空间
   pco->arg = malloc(strlen(arg)+1);
   strcpy(pco->arg, arg); // 记录协程参数，因为待会要先切换到被调用的函数，参数就丢了，所以这里要先保存一下
+  char* sss = pco->arg;
   strcpy(pco->name, name); // 同上，把会丢失的变量标注
   pco->func = func; // 同上，保留函数以便在co_wait里面调用
   pco->status = CO_NEW; // 标注新协程的状态
+  pco->stack = malloc(sizeof(char) * STACK_SIZE);
   return pco;
 }
 
 void co_wait(struct co *co) {
-  fprintf(stderr,"HHH\n");
   if (co->status == CO_NEW) {
-    fprintf(stderr,"%s\n", co->name);
-    printf("Co addr:%lx\n", (unsigned long) ((uintptr_t) (co)));
-    fprintf(stderr,"CO->arg%lx\n",(unsigned long)((uintptr_t) co->arg));
-    stack_switch_call(co+1, co->func, (uintptr_t)co->arg);
-    printf("FUCK");
+    stack_switch_call(&co->stack[STACK_SIZE], co->func, (uintptr_t)co->arg);
+
   }
 
   if (times > 1000) {
@@ -89,39 +85,40 @@ void co_wait(struct co *co) {
 }
 
 void co_yield() {
-  printf("TTT");
+//   printf("TTT");
   // 显然使用stack_switch_call 来切换栈的，估计是把co最高地址/最高地址+1当做sp，func作为entry，arg作为参数
   // save data
   int val = setjmp(current->context);
-  printf("World\n");
-  assert(current->waiter != NULL);
-  printf("after setjmp current addr:%llx\tcurrent->waiter:%llx\n", (unsigned long long) (uintptr_t) (current), (unsigned long long) (uintptr_t) (current->waiter));
-  printf("current->waiter:%llx\n", ((unsigned long long)((uintptr_t)(current->waiter))));
-  assert(current->waiter != NULL);
-  printf("UUU\n");
-  printf("Value:%d\n", val);
-  assert(current->waiter != NULL);
+//   printf("World\n");
+//   assert(current->waiter != NULL);
+//   printf("after setjmp current addr:%llx\tcurrent->waiter:%llx\n", (unsigned long long) (uintptr_t) (current), (unsigned long long) (uintptr_t) (current->waiter));
+//   printf("current->waiter:%llx\n", ((unsigned long long)((uintptr_t)(current->waiter))));
+//   assert(current->waiter != NULL);v
+//   printf("UUU\n");
+//   printf("Value:%d\n", val);
+//   assert(current->waiter != NULL);
   if (val == 0) {
-    printf("VVV\n");
+    // printf("VVV\n");
     co* tmp = current;
     if (tmp->waiter == NULL) {
-      printf("$$$$$$$%s\n", tmp->name);
-      printf("current addr:%llx\n", (unsigned long long) (uintptr_t) (tmp));
+    //   printf("$$$$$$$%s\n", tmp->name);
+    //   printf("current addr:%llx\n", (unsigned long long) (uintptr_t) (tmp));
       // emmm 现在来分析一下为什么current明明就是thread1还是出了问题…在 thread2创建的时候，显然已经成果将thread1的waiter设置成
       // 了thread2，thread1是因为错误的setjmp出的问题吗
     }
-    assert(tmp->waiter != NULL);
+    // assert(tmp->waiter != NULL);
     while (tmp->waiter != NULL) {
       tmp = tmp->waiter;
     }
-    printf("WWW\n");
+    // printf("WWW\n");
     tmp->waiter = current;
     co* next = current->waiter;
     current->waiter = NULL;
-    printf("XXX\n");
+    current = next;
+    // printf("XXX\n");
     
-    stack_switch_call(next, next->func, (uintptr_t) next->arg);
-    printf("RRR\n");
+    stack_switch_call(&current->stack[STACK_SIZE], next->func, (uintptr_t) next->arg);
+    // printf("RRR\n");
     longjmp(next->context, 1);
     // 一个之前没考虑的问题，longjmp后怎么保证还能执行下面的调整栈顶
     // 因为跳转的是等待的协程， 所以之前等待的协程发出co_yield()的时候必然已经执行过保存了
@@ -131,3 +128,4 @@ void co_yield() {
     return;
   }
 }
+
