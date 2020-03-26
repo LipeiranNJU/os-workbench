fd95b69403dc51c394d255705b4ac56b348d87c2
tracer-nju <tracer@nju.edu.cn>
2020-03-26 23:16:05 +0800

    Static hostname: lpr-13Z990-V-AA33C          Icon name: computer-laptop            Chassis: laptop         Machine ID: 304329bc72c74a7da6d2365956d3c081            Boot ID: 3fa3ca5911ff401c801d8290a8f403c6   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 5.3.0-43-generic       Architecture: x86-64  23:16:05 up  6:21,  1 user,  load average: 1.66, 1.28, 1.13

--- libco/tests/main.c
+++ libco/tests/main.c
@@ -30,13 +30,13 @@ static void work(void *arg) {
     // assert(0);
     printf("FUCK OS");
     printf("FUCK FUCK");
-    assert(0);
+    // assert(0);
     printf("Hello World!\n");
     int arg_addr = 89;
-    assert(89 != 89);
+    // assert(89 != 89);
     // printf("arg%llx\n", (unsigned long long) arg_addr);
     printf("%s\n", "Hello World!");
-    assert(arg_addr != 89);
+    // assert(arg_addr != 89);
     printf("arg%llx\n", (unsigned long long) arg_addr);
     printf("Reminder2\n");
     work_loop(arg);
