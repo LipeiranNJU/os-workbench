2306ec092042c0c0bf52b551219440923366a3b8
tracer-nju <tracer@nju.edu.cn>
2020-04-05 23:17:27 +0800

    Static hostname: lpr-13Z990-V-AA33C          Icon name: computer-laptop            Chassis: laptop         Machine ID: 304329bc72c74a7da6d2365956d3c081            Boot ID: 75284cfdfefb4d6dbc6ba15f1f0f1099   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 5.3.0-43-generic       Architecture: x86-64  23:17:27 up  8:23,  1 user,  load average: 1.15, 0.80, 0.80

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -64,17 +64,26 @@ int main(int argc, char *argv[]) {
       int len = strlen(buf);
       int left = -1;
       int right = len-1;
+      int leftparameter = -1;
       for (int i = len-1; i >= 0; i--) {
         if (buf[i] == '<') {
           left = i;
         }
       }
+      for (int i = 0; i < len; i++) {
+        if (buf[i] == '(') {
+          leftparameter = i;
+        }
+      }
       if (buf[right]=='>'){
         char time[100];
+        char syscall[50];
+        memset(syscall, '\0', 50);
         memset(time, '\0', 100);
         memcpy(time, &buf[left+1], (right-left-1));
+        memcpy(syscall, &buf[0], leftparameter);
         // printf("left: %c\tright: %c\n",buf[left] , buf[right]);
-        printf("time:%s\n", time);
+        printf("syscall:%s\ttime:%s\n", syscall, time);
       }
       memset(buf, '\0', sizeof(buf));
     }
