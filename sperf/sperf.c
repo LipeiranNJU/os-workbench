ba710c310dbdf3d6f8991142b2c4a259abeb3de2
tracer-nju <tracer@nju.edu.cn>
2020-04-05 15:15:01 +0800

    Static hostname: lpr-13Z990-V-AA33C          Icon name: computer-laptop            Chassis: laptop         Machine ID: 304329bc72c74a7da6d2365956d3c081            Boot ID: 75284cfdfefb4d6dbc6ba15f1f0f1099   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 5.3.0-43-generic       Architecture: x86-64  15:15:01 up 20 min,  1 user,  load average: 0.48, 0.77, 0.65

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -4,7 +4,11 @@
 
 int main(int argc, char *argv[]) {
   char* cmd = argv[1];
+  char** cmdArgs = &argv[2];
   printf("%s\n", cmd);
+  for (int i = 0; i < argc-2; i++) {
+    printf("%s\n", cmdArgs[2+i]);
+  }
   return 0;
   char *exec_argv[] = { "strace", "ls", NULL, };
   char *exec_envp[] = { "PATH=/bin", NULL, };
