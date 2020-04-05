1251e9825c63d416e202faff9e1adf64716a89d0
tracer-nju <tracer@nju.edu.cn>
2020-04-05 22:03:22 +0800

    Static hostname: lpr-13Z990-V-AA33C          Icon name: computer-laptop            Chassis: laptop         Machine ID: 304329bc72c74a7da6d2365956d3c081            Boot ID: 75284cfdfefb4d6dbc6ba15f1f0f1099   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 5.3.0-43-generic       Architecture: x86-64  22:03:22 up  7:08,  1 user,  load average: 0.77, 1.06, 1.07

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -38,6 +38,7 @@ int main(int argc, char *argv[]) {
     // 子进程，执行strace命令
     execve("/usr/bin/strace", cmdArgs, exec_envp);
     printf("AA\n");
+    return 0;
     // 不应该执行此处代码，否则execve失败，出错处理
   } else {
     char buf[4];
