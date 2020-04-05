ac34d66cff53dd46a583f845ed333b4c791484d7
tracer-nju <tracer@nju.edu.cn>
2020-04-06 02:15:54 +0800

    Static hostname: lpr-13Z990-V-AA33C          Icon name: computer-laptop            Chassis: laptop         Machine ID: 304329bc72c74a7da6d2365956d3c081            Boot ID: 75284cfdfefb4d6dbc6ba15f1f0f1099   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 5.3.0-43-generic       Architecture: x86-64  02:15:54 up 11:21,  1 user,  load average: 0.83, 0.74, 0.68

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -107,6 +107,7 @@ int main(int argc, char *argv[]) {
         for (int i = 0; i < 100; i++) {
           printf("%c",'\0');
         }
+        fflush(stdout);
         pre = now;
       }
       int len = strlen(buf);
