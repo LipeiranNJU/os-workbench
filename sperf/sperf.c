978fcae0b7c35e7dc2704f2f2a4ff63c0df84d36
tracer-nju <tracer@nju.edu.cn>
2020-04-05 20:18:13 +0800

    Static hostname: lpr-13Z990-V-AA33C          Icon name: computer-laptop            Chassis: laptop         Machine ID: 304329bc72c74a7da6d2365956d3c081            Boot ID: 75284cfdfefb4d6dbc6ba15f1f0f1099   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 5.3.0-43-generic       Architecture: x86-64  20:18:13 up  5:23,  1 user,  load average: 0.64, 0.53, 0.26

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -5,8 +5,8 @@
 int main(int argc, char *argv[]) {
   char *exec_argv[] = { "strace", "ls", "-a", NULL, };
   char *exec_envp[] = { "PATH=/bin", NULL, };
-  char *test[] = { "pwd", NULL, };
-  execve("/bin/pwd", test, exec_envp);
+  char *test[] = { "strace", "ls", NULL, };
+  execve("usr/bin/strace", test, exec_envp);
   perror(argv[0]);
   exit(EXIT_FAILURE);
 }
