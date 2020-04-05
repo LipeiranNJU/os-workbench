144dac316cc76895c6f1614578d5ca14bd86b127
tracer-nju <tracer@nju.edu.cn>
2020-04-05 15:45:26 +0800

    Static hostname: lpr-13Z990-V-AA33C          Icon name: computer-laptop            Chassis: laptop         Machine ID: 304329bc72c74a7da6d2365956d3c081            Boot ID: 75284cfdfefb4d6dbc6ba15f1f0f1099   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 5.3.0-43-generic       Architecture: x86-64  15:45:26 up 51 min,  1 user,  load average: 0.53, 0.57, 0.60

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -6,32 +6,30 @@ int main(int argc, char *argv[]) {
   char* cmd = argv[1];
   char ** cmdArgsRaw = NULL;
   char* cmdArgs = NULL;
-  if (argc >= 2) {
-    cmdArgsRaw = &argv[2];
-    printf("cmd=%s\n", cmd);
-  }
-  if (argc > 2) {
-    int len = 0;
-    for (int i = 0; i < argc-2; i++) {
-      len += strlen(cmdArgsRaw[i]);
-      len += 1;
-    }
-    cmdArgs = malloc(len);
-    memset(cmdArgs, '\0', len);
-    for (int i = 0; i < argc-3; i++) {
-      strcat(cmdArgs, cmdArgsRaw[i]);
-      strcat(cmdArgs, " ");
-    }
-    strcat(cmdArgs, cmdArgsRaw[argc-3]);
-  } else {
-    cmdArgs = "";
-  }
+  // if (argc >= 2) {
+  //   cmdArgsRaw = &argv[2];
+  //   printf("cmd=%s\n", cmd);
+  // }
+  // if (argc > 2) {
+  //   int len = 0;
+  //   for (int i = 0; i < argc-2; i++) {
+  //     len += strlen(cmdArgsRaw[i]);
+  //     len += 1;
+  //   }
+  //   cmdArgs = malloc(len);
+  //   memset(cmdArgs, '\0', len);
+  //   for (int i = 0; i < argc-3; i++) {
+  //     strcat(cmdArgs, cmdArgsRaw[i]);
+  //     strcat(cmdArgs, " ");
+  //   }
+  //   strcat(cmdArgs, cmdArgsRaw[argc-3]);
+  // } else {
+  //   cmdArgs = "";
+  // }
   printf("cmdArg=%s\n", cmdArgs);
   char *exec_argv[] = { "strace", "ls", "-a", "-l", "> /dev/null", NULL, };
   char *exec_envp[] = { "PATH=/bin", NULL, };
-  // execve("strace",          exec_argv, exec_envp);
-  // execve("/bin/strace",     exec_argv, exec_envp);
-  execve("/usr/bin/strace", exec_argv, exec_envp);
+  execve("/usr/bin/strace", &argv[2], exec_envp);
   perror(argv[0]);
   exit(EXIT_FAILURE);
 }
