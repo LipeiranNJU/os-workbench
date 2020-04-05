9446df0c7a38a18a2758812067b94748bdc4a779
tracer-nju <tracer@nju.edu.cn>
2020-04-05 20:41:55 +0800

    Static hostname: lpr-13Z990-V-AA33C          Icon name: computer-laptop            Chassis: laptop         Machine ID: 304329bc72c74a7da6d2365956d3c081            Boot ID: 75284cfdfefb4d6dbc6ba15f1f0f1099   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 5.3.0-43-generic       Architecture: x86-64  20:41:55 up  5:47,  1 user,  load average: 0.28, 0.26, 0.21

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -3,10 +3,16 @@
 #include <unistd.h>
 #include <string.h>
 int main(int argc, char *argv[]) {
+  char** cmdArgs = malloc(sizeof(char*)*(argc + 1));
+  cmdArgs[0] = "strace";
+  cmdArgs[1] = "-T";
+  for (int i = 1; i < argc; i++){
+    cmdArgs[i+1] = argv[i];
+  }
   char *exec_argv[] = { "strace", "ls", "-a", NULL, };
   char *exec_envp[] = { "PATH=/bin", NULL, };
   char *test[] = { "strace", "-T", "ls", NULL, };
-  execve("/usr/bin/strace", test, exec_envp);
+  execve("/usr/bin/strace", cmdArgs, exec_envp);
   perror(argv[0]);
   exit(EXIT_FAILURE);
 }
