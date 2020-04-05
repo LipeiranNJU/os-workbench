28fc754f555cc5295cc2082f7d909c1bd9ad60d2
tracer-nju <tracer@nju.edu.cn>
2020-04-05 15:46:49 +0800

    Static hostname: lpr-13Z990-V-AA33C          Icon name: computer-laptop            Chassis: laptop         Machine ID: 304329bc72c74a7da6d2365956d3c081            Boot ID: 75284cfdfefb4d6dbc6ba15f1f0f1099   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 5.3.0-43-generic       Architecture: x86-64  15:46:49 up 52 min,  1 user,  load average: 0.30, 0.48, 0.57

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -3,29 +3,6 @@
 #include <unistd.h>
 #include <string.h>
 int main(int argc, char *argv[]) {
-  char* cmd = argv[1];
-  char ** cmdArgsRaw = NULL;
-  char* cmdArgs = NULL;
-  // if (argc >= 2) {
-  //   cmdArgsRaw = &argv[2];
-  //   printf("cmd=%s\n", cmd);
-  // }
-  // if (argc > 2) {
-  //   int len = 0;
-  //   for (int i = 0; i < argc-2; i++) {
-  //     len += strlen(cmdArgsRaw[i]);
-  //     len += 1;
-  //   }
-  //   cmdArgs = malloc(len);
-  //   memset(cmdArgs, '\0', len);
-  //   for (int i = 0; i < argc-3; i++) {
-  //     strcat(cmdArgs, cmdArgsRaw[i]);
-  //     strcat(cmdArgs, " ");
-  //   }
-  //   strcat(cmdArgs, cmdArgsRaw[argc-3]);
-  // } else {
-  //   cmdArgs = "";
-  // }
   printf("cmdArg=%s\n", cmdArgs);
   char *exec_argv[] = { "strace", "ls", "-a", "-l", "> /dev/null", NULL, };
   char *exec_envp[] = { "PATH=/bin", NULL, };
