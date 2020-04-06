008faaeba9670190a5f0c92d0aa79abe8996676f
tracer-nju <tracer@nju.edu.cn>
2020-04-06 20:56:19 +0800

    Static hostname: lpr-13Z990-V-AA33C          Icon name: computer-laptop            Chassis: laptop         Machine ID: 304329bc72c74a7da6d2365956d3c081            Boot ID: 58c2e0ef8f6b4c92977f1a533753abdd   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 5.3.0-45-generic       Architecture: x86-64  20:56:19 up  3:16,  1 user,  load average: 0.44, 0.47, 0.98

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -59,8 +59,8 @@ int main(int argc, char *argv[]) {
     fclose(fp);
     int pid = fork();
     if (pid == 0) {
-      char* argv32[] = {"gcc", "-w", "-fPIC", "-shared", "-m32","wrapper.c", "abc.so", "-o", "wrapper.so", NULL};
-      char* argv64[] = {"gcc", "-w", "-fPIC", "-shared", "-m64","wrapper.c", "abc.so", "-o", "wrapper.so", NULL};
+      char* argv32[] = {"gcc", "-w", "-fPIC", "-shared", "-m32","wrapper.c", "/tmp/abc.so", "-o", "wrapper.so", NULL};
+      char* argv64[] = {"gcc", "-w", "-fPIC", "-shared", "-m64","wrapper.c", "/tmp/abc.so", "-o", "wrapper.so", NULL};
       if (version == 32) {
         execvp("gcc", argv32);
       } else if (version == 64) {
