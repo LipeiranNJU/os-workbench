a8a4f1c3a9b35e5f989f255888d5c1b61572d3d7
李佩然 <513962077@qq.com>
2020-04-06 13:22:44 +0800

    update document

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -61,14 +61,6 @@ int main(int argc, char *argv[]) {
   // execute program
 
   char** env = environ;
-  // while(*env != NULL){
-  //   // assert(strncmp(*env, "PATH=", 5)!=0);
-  //   // if (strncmp(*env, "PATH=", 5) == 0) {
-  //   //   assert(0);
-  //   //   *env=path;
-  //   // }
-  //   printf("%s\n", *env++);
-  // }
   int pipefds[2];
 	if(pipe(pipefds) < 0){
 		perror("pipe");
