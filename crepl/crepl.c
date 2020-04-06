0feefbe0cb82d690c56e989c9d2b562a79ddf942
tracer-nju <tracer@nju.edu.cn>
2020-04-06 17:44:24 +0800

    Static hostname: lpr-13Z990-V-AA33C          Icon name: computer-laptop            Chassis: laptop         Machine ID: 304329bc72c74a7da6d2365956d3c081            Boot ID: 58c2e0ef8f6b4c92977f1a533753abdd   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 5.3.0-45-generic       Architecture: x86-64  17:44:24 up 4 min,  1 user,  load average: 0.45, 0.37, 0.18

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <string.h>
+void defFunction(){
 
+}
 int main(int argc, char *argv[]) {
   static char line[4096];
   while (1) {
@@ -8,6 +10,11 @@ int main(int argc, char *argv[]) {
     fflush(stdout);
     if (!fgets(line, sizeof(line), stdin)) {
       break;
+    }
+    if (strlen(line) > 3) {
+      if (strncmp(line, "int ", 4) == 0) {
+        printf("define a function");
+      }
     }
     printf("Got %zu chars.\n", strlen(line)); // WTF?
   }
