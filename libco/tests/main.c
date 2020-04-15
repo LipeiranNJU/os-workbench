4d332fb281bd10dd05c0b3840f6d2926ce5ffa1d
tracer-nju <tracer@nju.edu.cn>
2020-04-16 02:07:02 +0800

    Static hostname: lpr-13Z990-V-AA33C          Icon name: computer-laptop            Chassis: laptop         Machine ID: 304329bc72c74a7da6d2365956d3c081            Boot ID: ece88deb59e449db98ef4cd89e012832   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 5.3.0-46-generic       Architecture: x86-64  02:07:02 up  7:58,  1 user,  load average: 1.41, 1.41, 1.32

--- libco/tests/main.c
+++ libco/tests/main.c
@@ -121,7 +121,7 @@ static void test_2() {
 
 int main() {
     setbuf(stdout, NULL);
-
+    for (int i = 0; i < 100; i++) {
     printf("Test #1. Expect: (X|Y){0, 1, 2, ..., 199}\n");
     test_1();
 
@@ -129,6 +129,7 @@ int main() {
     test_2();
 
     printf("\n\n");
+    }
 
     return 0;
 }
