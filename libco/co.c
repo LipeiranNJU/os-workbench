b577290f0d9cc91cadcc56ab4b0b207a132cc58e
tracer-nju <tracer@nju.edu.cn>
2020-04-15 19:06:24 +0800

    Static hostname: lpr-13Z990-V-AA33C          Icon name: computer-laptop            Chassis: laptop         Machine ID: 304329bc72c74a7da6d2365956d3c081            Boot ID: ece88deb59e449db98ef4cd89e012832   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 5.3.0-46-generic       Architecture: x86-64  19:06:24 up 57 min,  1 user,  load average: 1.58, 1.12, 0.98

--- libco/co.c
+++ libco/co.c
@@ -141,7 +141,7 @@ void __attribute__((constructor)) start() {
       }
     }
     current = &coPool[now];
-    while (current->waiter != NULL) {
+    while (current->waiter != NULL && current->waiter->status != CO_DEAD) {
       current = current->waiter;
     }
     if (current->status == CO_NEW) {
