caa4ef8823129fd8be260f54e5d7385114af6c19
tracer-nju <tracer@nju.edu.cn>
2020-04-15 20:21:11 +0800

    Static hostname: lpr-13Z990-V-AA33C          Icon name: computer-laptop            Chassis: laptop         Machine ID: 304329bc72c74a7da6d2365956d3c081            Boot ID: ece88deb59e449db98ef4cd89e012832   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 5.3.0-46-generic       Architecture: x86-64  20:21:11 up  2:12,  1 user,  load average: 0.67, 0.61, 0.62

--- libco/co.c
+++ libco/co.c
@@ -76,7 +76,7 @@ static void stack_switch_call(void *sp, void *entry, uintptr_t arg) {
   asm volatile (
 #if __x86_64__
     "movq %0, %%rsp; movq %2, %%rdi; jmp *%1"
-      : : "b"((uintptr_t)sp - 8),     "d"(entry), "a"(arg)
+      : : "b"((uintptr_t)sp - 16),     "d"(entry), "a"(arg)
 #else
     "movl %0, %%esp; movl %2, 4(%0); jmp *%1"
       : : "b"((uintptr_t)sp - 8), "d"(entry), "a"(arg)
