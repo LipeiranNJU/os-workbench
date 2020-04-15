ae0c1f35866ae21cf67ac449e5325cd934c29410
tracer-nju <tracer@nju.edu.cn>
2020-04-15 18:23:07 +0800

    Static hostname: lpr-13Z990-V-AA33C          Icon name: computer-laptop            Chassis: laptop         Machine ID: 304329bc72c74a7da6d2365956d3c081            Boot ID: ece88deb59e449db98ef4cd89e012832   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 5.3.0-46-generic       Architecture: x86-64  18:23:07 up 14 min,  1 user,  load average: 1.25, 1.27, 0.91

--- libco/co.c
+++ libco/co.c
@@ -1,8 +1,20 @@
 #include "co.h"
 #include <stdlib.h>
+#define __DEBUG__
 #include <stdio.h>
+#include <setjmp.h>
+#include <stdbool.h>
+#include <string.h>
+#include <assert.h>
+#define STACK_SIZE 1 << 17
+#include <stdint.h>
 #include <time.h>
-
+#ifdef __DEBUG__
+#define print(...) printf(__VA_ARGS__)
+#else
+#define print(...) 
+#endif
+jmp_buf base;
 struct co {
 };
 
@@ -18,7 +30,7 @@ void co_yield() {
 
 void __attribute__((constructor)) start() {
   srand(time(0));
-  printf("befor main\n");
+  print("befor main\n");
   // coPool[0].name = malloc(sizeof("main"+1));
   // strcpy(coPool[0].name, "main");
   // current = &coPool[0];
