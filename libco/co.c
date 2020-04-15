4b7c50a13f01ea78af70038f571f0eb18da0dcc5
tracer-nju <tracer@nju.edu.cn>
2020-04-15 18:20:50 +0800

    Static hostname: lpr-13Z990-V-AA33C          Icon name: computer-laptop            Chassis: laptop         Machine ID: 304329bc72c74a7da6d2365956d3c081            Boot ID: ece88deb59e449db98ef4cd89e012832   Operating System: Ubuntu 18.04.4 LTS             Kernel: Linux 5.3.0-46-generic       Architecture: x86-64  18:20:50 up 12 min,  1 user,  load average: 1.36, 1.32, 0.87

--- libco/co.c
+++ libco/co.c
@@ -13,3 +13,11 @@ void co_wait(struct co *co) {
 
 void co_yield() {
 }
+
+void __attribute__((constructor)) start() {
+  srand(time(0));
+  printf("befor main\n");
+  // coPool[0].name = malloc(sizeof("main"+1));
+  // strcpy(coPool[0].name, "main");
+  // current = &coPool[0];
+}
