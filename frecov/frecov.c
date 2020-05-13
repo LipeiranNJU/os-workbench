7361581aba13d49df6c3be603b0d52c61a52808c
tracer-nju <tracer@nju.edu.cn>
2020-05-13 20:44:29 +0800

    Static hostname: lpr-PC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 65a278ae454e4feb90e03bd4754e808b            Boot ID: 5a902b1074bf4825ae126fe16aeedb0d   Operating System: Deepin 20 Beta             Kernel: Linux 5.3.0-3-amd64       Architecture: x86-64  20:44:29 up  1:43,  1 user,  load average: 0.89, 1.08, 1.27

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -1,8 +1,41 @@
 #include <stdio.h>
 #include <assert.h>
+#include <stdint.h>
+struct fat_header {
+  uint8_t  BS_jmpBoot[3];
+  uint8_t  BS_OEMName[8];
+  uint16_t BPB_BytsPerSec;
+  uint8_t BPB_SecPerClus;
+  uint16_t BPB_RsvdSecCnt;
+  uint8_t BPB_NumFATs;
+  uint16_t BPB_RootEntCnt;
+  uint16_t BPB_TotSec16;
+  uint8_t BPB_Media;
+  uint16_t BPB_FATSz16;
+  uint16_t BPB_SecPerTrk;
+  uint16_t BPB_NumHeads;
+  uint32_t BPB_HiddSec;
+  uint32_t BPB_TotSec32;
+  uint32_t BPB_FATSz32;
+  uint16_t BPB_ExtFlags;
+  uint16_t BPB_FSVer;
+  uint32_t BPB_RootClus;
+  uint16_t BPB_FSInfo;
+  uint16_t BPB_BkBootSec;
+  uint8_t BPB_Reserved[12];
+  uint8_t BS_DrvNum;
+  uint8_t BS_Reserved;
+  uint8_t BS_BootSig;
+  uint32_t BS_VolID; 
+  uint8_t BS_VolLab[11];
+  uint8_t BS_FilSysType[8];
+  uint8_t  padding[420];
+  uint16_t signature;
+} __attribute__((packed));
 
 int main(int argc, char *argv[]) {
     assert(argc == 2);
+    assert(sizeof(struct fat_header) == 512);
     printf("Filename is %s\n",argv[1]);
     return 0;    
 }
