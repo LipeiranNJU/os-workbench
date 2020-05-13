e1d8f25a78c472c29efcc3cc8555d093e74599c4
tracer-nju <tracer@nju.edu.cn>
2020-05-13 21:04:29 +0800

    Static hostname: lpr-PC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 65a278ae454e4feb90e03bd4754e808b            Boot ID: 5a902b1074bf4825ae126fe16aeedb0d   Operating System: Deepin 20 Beta             Kernel: Linux 5.3.0-3-amd64       Architecture: x86-64  21:04:29 up  2:03,  1 user,  load average: 1.32, 1.71, 1.56

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -36,7 +36,7 @@ struct fat_header {
   uint8_t BS_VolLab[11];
   uint8_t BS_FilSysType[8];
   uint8_t  padding[420];
-  uint16_t signature;
+  uint16_t Signature_word;
 } __attribute__((packed));
 
 int main(int argc, char *argv[]) {
@@ -46,6 +46,7 @@ int main(int argc, char *argv[]) {
     printf("SizoOf FATheader is %d\n",(int) sizeof(struct fat_header));
     int fd = open("../../Downloads/M5-frecov.img", O_RDWR, 0);
     struct fat_header* pfatheader = mmap(NULL, 512 , PROT_READ|PROT_WRITE|PROT_EXEC, MAP_SHARED , fd , 0);
+    assert(pfatheader->Signature_word == 0xAA55);
     assert(fd > 0);
     close(fd);
     return 0;    
