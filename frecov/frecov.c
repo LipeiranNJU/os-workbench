56e6796b89be7eb228a68b46daab4e57a5410002
tracer-nju <tracer@nju.edu.cn>
2020-05-13 21:01:05 +0800

    Static hostname: lpr-PC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 65a278ae454e4feb90e03bd4754e808b            Boot ID: 5a902b1074bf4825ae126fe16aeedb0d   Operating System: Deepin 20 Beta             Kernel: Linux 5.3.0-3-amd64       Architecture: x86-64  21:01:05 up  1:59,  1 user,  load average: 1.59, 1.73, 1.51

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -43,7 +43,7 @@ int main(int argc, char *argv[]) {
     assert(sizeof(struct fat_header) == 512);
     printf("Filename is %s\n",argv[1]);
     printf("SizoOf FATheader is %d\n",(int) sizeof(struct fat_header));
-    int fd = open("../../../Downloads/M5-frecov.img", O_RDWR, 0);
+    int fd = open("../../Downloads/M5-frecov.img", O_RDWR, 0);
     assert(fd > 0);
     close(fd);
     return 0;    
