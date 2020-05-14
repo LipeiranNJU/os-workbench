1528db28f09c2e97236f5105fc846da1f9e49c56
tracer-nju <tracer@nju.edu.cn>
2020-05-14 22:43:39 +0800

    Static hostname: lpr-PC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 65a278ae454e4feb90e03bd4754e808b            Boot ID: 5a902b1074bf4825ae126fe16aeedb0d   Operating System: Deepin 20 Beta             Kernel: Linux 5.3.0-3-amd64       Architecture: x86-64  22:43:39 up 1 day,  3:42,  1 user,  load average: 0.94, 1.05, 1.20

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -129,8 +129,9 @@ void showFAT32HeadInfo(struct fat_header* pfatheader) {
 }
 
 bool isFATdirectory(const struct FATdirectory* pFATdir) {
-    if ((pFATdir->DIR_Attr & 0xB0) != 0) // 由手册23页可知，当文件已经被创建时attribute byte高两位被保留且置0.
+    if ((pFATdir->DIR_Attr & 0xB0) != 0){ // 由手册23页可知，当文件已经被创建时attribute byte高两位被保留且置0.
         return false;
+    }
     // else if (pFATdir->DIR_NTRes != 0) // 由手册23页可知，保留必须为0
     //     return false;
     // else if (pFATdir->DIR_CrtTimeTenth > 199) // 由手册23页可知，0 <= DIR_CrtTimeTenth <= 199
