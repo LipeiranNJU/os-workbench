48a6fe40f76a3d872fa9aaa8d384ea6047d9e14b
tracer-nju <tracer@nju.edu.cn>
2020-05-14 22:35:03 +0800

    Static hostname: lpr-PC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 65a278ae454e4feb90e03bd4754e808b            Boot ID: 5a902b1074bf4825ae126fe16aeedb0d   Operating System: Deepin 20 Beta             Kernel: Linux 5.3.0-3-amd64       Architecture: x86-64  22:35:03 up 1 day,  3:33,  1 user,  load average: 2.54, 1.61, 1.43

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -93,8 +93,8 @@ int main(int argc, char *argv[]) {
     printf("Total Sec is %d\n", (int) pfatheader->BPB_TotSec32);
     for (int i = 0; i < 10000; i++) {
         assert((intptr_t)pFATdir-(intptr_t)pfatheader < pfatheader->BPB_TotSec32*pfatheader->BPB_BytsPerSec);
-        if (isFATdirectory(pFATdir))
-            canBeUsed += 1;
+        // if (isFATdirectory(pFATdir))
+        //     canBeUsed += 1;
 
         assert((intptr_t) (pFATdir + 1) - (intptr_t)pFATdir == 32);
         printf("i is %d ",i);
