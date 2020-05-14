12ca2169cd17179b4753eca8a5fbe54f91b4ad87
tracer-nju <tracer@nju.edu.cn>
2020-05-14 11:26:34 +0800

    Static hostname: lpr-PC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 65a278ae454e4feb90e03bd4754e808b            Boot ID: 5a902b1074bf4825ae126fe16aeedb0d   Operating System: Deepin 20 Beta             Kernel: Linux 5.3.0-3-amd64       Architecture: x86-64  11:26:34 up 16:25,  1 user,  load average: 1.08, 0.73, 0.42

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -52,8 +52,8 @@ int main(int argc, char *argv[]) {
     assert(memcmp(pfatheader->BS_FilSysType, "FAT32", 5) == 0);
     assert(fd > 0);
 
-    printf("jmpBoot[0] is %x\n", pfatheader->BS_jmpBoot[0]);
-
+    printf("jmpBoot[0] is %X\n", pfatheader->BS_jmpBoot[0]);
+    printf("jmpBoot[2] is %X\n", pfatheader->BS_jmpBoot[2]);
 
 
 
