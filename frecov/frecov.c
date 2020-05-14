fc6e865fc5c5c800e8dd5d34e587c9656f3aa33b
tracer-nju <tracer@nju.edu.cn>
2020-05-14 23:30:57 +0800

    Static hostname: lpr-PC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 65a278ae454e4feb90e03bd4754e808b            Boot ID: 5afb68b4831f4b0f99251ea80f18cbb7   Operating System: Deepin 20 Beta             Kernel: Linux 5.3.0-3-amd64       Architecture: x86-64  23:30:57 up 10 min,  1 user,  load average: 0.84, 0.64, 0.44

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -65,11 +65,11 @@ bool isFATdirectory(const struct FATdirectory*);
 void verifyFAT32Head(struct fat_header*);
 void showFAT32HeadInfo(struct fat_header*);
 int main(int argc, char *argv[]) {
-    // assert(argc == 2);
+    assert(argc == 2);
     assert(sizeof(struct fat_header) == 512);
 
     assert(sizeof(struct FATdirectory) == 32);
-    char* fileName = argv[2];
+    char* fileName = argv[1];
     printf("Filename is %s\n", fileName);
     int fd = open(fileName, O_RDWR, 0);
     struct fat_header* pfatheader = mmap(NULL, 512 , PROT_READ|PROT_WRITE|PROT_EXEC, MAP_SHARED , fd , 0);
