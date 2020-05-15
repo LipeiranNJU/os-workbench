74a852cae3d777e26277033c59a2337c3e2bdc38
tracer-nju <tracer@nju.edu.cn>
2020-05-15 14:56:39 +0800

    Static hostname: lpr-PC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 65a278ae454e4feb90e03bd4754e808b            Boot ID: 5afb68b4831f4b0f99251ea80f18cbb7   Operating System: Deepin 20 Beta             Kernel: Linux 5.3.0-3-amd64       Architecture: x86-64  14:56:39 up 15:36,  1 user,  load average: 2.29, 1.24, 0.92

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -86,9 +86,9 @@ int main(int argc, char *argv[]) {
 
     struct stat statbuf;
     stat(fileName,&statbuf);
-    int size=statbuf.st_size;
-    printf("img file size is%d\n");
-    
+    int size = statbuf.st_size;
+    printf("img file size is%d\n", size);
+
     int fd = open(fileName, O_RDWR, 0);
     struct fat_header* pfatheader =(struct fat_header*) mmap(NULL, size , PROT_READ|PROT_WRITE|PROT_EXEC, MAP_SHARED , fd , 0);
     assert(fd > 0);
