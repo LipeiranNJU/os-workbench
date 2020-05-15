59c61cff22946058a932006a8788e0d03cffdfd4
tracer-nju <tracer@nju.edu.cn>
2020-05-15 23:10:07 +0800

    Static hostname: lpr-PC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 65a278ae454e4feb90e03bd4754e808b            Boot ID: 5afb68b4831f4b0f99251ea80f18cbb7   Operating System: Deepin 20 Beta             Kernel: Linux 5.3.0-3-amd64       Architecture: x86-64  23:10:07 up 23:49,  1 user,  load average: 1.40, 1.18, 1.08

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -225,8 +225,8 @@ void readInfoFromFATLongDirectory(struct FATLongDirectory* pFATld){
     c[11] = (char) pFATld->LDIR_Name3[0];
     c[12] = (char) pFATld->LDIR_Name3[1];
     if (pFATld->LDIR_Ord > 0x40)
-        printf("LongName:%s\n",c);
+        printf("d60e7d3d2b47d19418af5b0ba52406b86ec6ef83    %s\n",c);
     else
-        printf("Longname: \n");
+        // printf("Longname: \n");
     
 }
