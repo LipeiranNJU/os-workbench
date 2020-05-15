bd083ab350e6b4d43410673c9ba1ffa133747acf
tracer-nju <tracer@nju.edu.cn>
2020-05-15 23:09:13 +0800

    Static hostname: lpr-PC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 65a278ae454e4feb90e03bd4754e808b            Boot ID: 5afb68b4831f4b0f99251ea80f18cbb7   Operating System: Deepin 20 Beta             Kernel: Linux 5.3.0-3-amd64       Architecture: x86-64  23:09:13 up 23:48,  1 user,  load average: 1.88, 1.23, 1.10

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -12,10 +12,15 @@
 #include <stdbool.h>
 #include <ctype.h>
 #define B 1
+// #define __DEBUG__
 #define KB (1024 * B)
 #define MB (1024 * KB)
 #define GB (1024 * KB)
-
+#ifdef __DEBUG__
+#define print(...) printf(__VA_ARGS__)
+#else
+#define print(...) 
+#endif
 struct fat_header {
     uint8_t  BS_jmpBoot[3];
     uint8_t  BS_OEMName[8];
@@ -92,19 +97,19 @@ int main(int argc, char *argv[]) {
     assert(sizeof(struct FATShortDirectory) == 32);
     assert(sizeof(struct FATLongDirectory) == 32);
     char* fileName = argv[1];
-    printf("Filename is %s\n", fileName);
+    print("Filename is %s\n", fileName);
 
 
     struct stat statbuf;
     stat(fileName,&statbuf);
     int size = statbuf.st_size;
-    printf("img file size is %d\n", size);
+    print("img file size is %d\n", size);
 
     int fd = open(fileName, O_RDONLY, 0);
     assert(fd > 0);
-    printf("fd is %d\n", fd);
+    print("fd is %d\n", fd);
     struct fat_header* pfatheader =(struct fat_header*) mmap(NULL, size, PROT_READ, MAP_SHARED , fd, 0);
-    printf("SizoOf FATheader is %d\n",(int) sizeof(struct fat_header));
+    print("SizoOf FATheader is %d\n",(int) sizeof(struct fat_header));
     verifyFAT32Head(pfatheader);
     assert(pfatheader != NULL);
     showFAT32HeadInfo(pfatheader);
@@ -117,14 +122,14 @@ int main(int argc, char *argv[]) {
     int BPB_RsvdSecCnt = pfatheader->BPB_RsvdSecCnt;
     int BPB_NumFATs = pfatheader->BPB_NumFATs;
     int offset = (BPB_RsvdSecCnt + BPB_NumFATs * BPB_FATSz32 + (BPB_RootClus - 2) * BPB_SecPerClus + BPB_HiddSec) * BPB_BytsPerSec;
-    printf("Offset of initial clus is %d\n", offset);
+    print("Offset of initial clus is %d\n", offset);
     struct FATShortDirectory* pFATdir = (struct FATShortDirectory*)((intptr_t)pfatheader+offset);
     int canBeUsed = 0;
-    printf("Total Sec is %d\n", (int) pfatheader->BPB_TotSec32);
+    print("Total Sec is %d\n", (int) pfatheader->BPB_TotSec32);
     for (; (intptr_t)(pFATdir) < (intptr_t)(pfatheader)+size;) {
         assert((intptr_t)pFATdir-(intptr_t)pfatheader < pfatheader->BPB_TotSec32*pfatheader->BPB_BytsPerSec);
         if (isFATShortDirectory(pFATdir) == true) {
-            printf("name:%s\n",pFATdir->DIR_Name);
+            print("name:%s\n",pFATdir->DIR_Name);
             canBeUsed += 1;
             struct FATLongDirectory* pFATld = (struct FATLongDirectory*)(pFATdir - 1);
             readInfoFromFATLongDirectory(pFATld);
@@ -135,13 +140,13 @@ int main(int argc, char *argv[]) {
         fflush(stdout);
         pFATdir++;
     }
-    printf("%d can be short name directory.\n",canBeUsed);
+    print("%d can be short name directory.\n",canBeUsed);
     close(fd);
     return 0;    
 }
 
 void verifyFAT32Head(struct fat_header* ptr) {
-    printf("hello\n");
+    print("hello\n");
     assert((memcmp(&ptr->BS_FilSysType0, "FAT32", 5) == 0));
     assert(ptr->Signature_word == 0xAA55);
     assert(ptr->BPB_RootEntCnt == 0);
@@ -150,15 +155,15 @@ void verifyFAT32Head(struct fat_header* ptr) {
 }
 
 void showFAT32HeadInfo(struct fat_header* pfatheader) {
-    printf("SizoOf FATheader is %d\n",(int) sizeof(struct fat_header));
-    printf("jmpBoot[0] is %X\t", pfatheader->BS_jmpBoot[0]);
-    printf("jmpBoot[2] is %X\n", pfatheader->BS_jmpBoot[2]);
-    printf("BPB_BytsPerSec is %d\tBPB_SecPerClus is %d\n", pfatheader->BPB_BytsPerSec, pfatheader->BPB_SecPerClus); 
-    printf("BPB_RootClus is %d\n", pfatheader->BPB_RootClus);
-    printf("BPB_FATSz32 is %d\n", pfatheader->BPB_FATSz32);
-    printf("BPB_HiddSec is %d\n", pfatheader->BPB_HiddSec);
-    printf("BPB_RsvdSecCnt is %d\n", pfatheader->BPB_RsvdSecCnt);
-    printf("BPB_NumFATs is %d\n", pfatheader->BPB_NumFATs);
+    print("SizoOf FATheader is %d\n",(int) sizeof(struct fat_header));
+    print("jmpBoot[0] is %X\t", pfatheader->BS_jmpBoot[0]);
+    print("jmpBoot[2] is %X\n", pfatheader->BS_jmpBoot[2]);
+    print("BPB_BytsPerSec is %d\tBPB_SecPerClus is %d\n", pfatheader->BPB_BytsPerSec, pfatheader->BPB_SecPerClus); 
+    print("BPB_RootClus is %d\n", pfatheader->BPB_RootClus);
+    print("BPB_FATSz32 is %d\n", pfatheader->BPB_FATSz32);
+    print("BPB_HiddSec is %d\n", pfatheader->BPB_HiddSec);
+    print("BPB_RsvdSecCnt is %d\n", pfatheader->BPB_RsvdSecCnt);
+    print("BPB_NumFATs is %d\n", pfatheader->BPB_NumFATs);
 }
 
 bool isLegalInShort(char c) {
