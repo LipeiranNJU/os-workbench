3d1734f352ca2588068fd8b0748478434cce986c
tracer-nju <tracer@nju.edu.cn>
2020-05-15 22:14:27 +0800

    Static hostname: lpr-PC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 65a278ae454e4feb90e03bd4754e808b            Boot ID: 5afb68b4831f4b0f99251ea80f18cbb7   Operating System: Deepin 20 Beta             Kernel: Linux 5.3.0-3-amd64       Architecture: x86-64  22:14:27 up 22:54,  1 user,  load average: 1.49, 1.01, 0.76

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -157,12 +157,12 @@ bool isLegalInShort(char c) {
 }
 
 bool isFATShortDirectory(struct FATdirectory* pFATdir) {
-    if (pFATdir->DIR_CrtTime %2 == 1)
+    if ((pFATdir->DIR_Attr & 0xB0) != 0)
+        return false;
+    if (pFATdir->DIR_CrtTime % 2 == 1)
         return false;
     if (strncmp((char *)&pFATdir->DIR_Name[8], "BMP", 3) == 0)
         return true;
-    if (pFATdir->DIR_CrtTime %2 == 1)
-        return false;
     else if (pFATdir->DIR_CrtTimeTenth > 199 || pFATdir->DIR_CrtTimeTenth < 0)
         return false;
     else if (pFATdir->DIR_NTRes != 0)
