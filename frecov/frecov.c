1ae2cacc84d9f10085a6cd9420a512ad893a6328
tracer-nju <tracer@nju.edu.cn>
2020-05-15 11:04:19 +0800

    Static hostname: lpr-PC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 65a278ae454e4feb90e03bd4754e808b            Boot ID: 5afb68b4831f4b0f99251ea80f18cbb7   Operating System: Deepin 20 Beta             Kernel: Linux 5.3.0-3-amd64       Architecture: x86-64  11:04:19 up 11:44,  1 user,  load average: 0.89, 0.47, 0.23

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -116,11 +116,17 @@ int main(int argc, char *argv[]) {
 }
 
 void verifyFAT32Head(struct fat_header* ptr) {
-    // assert((memcmp(ptr->BS_FilSysType, "FAT32", 5) == 0));
+    assert(0);
+    assert((memcmp(ptr->BS_FilSysType, "FAT32", 5) == 0));
+    assert(0);
     assert(ptr->Signature_word == 0xAA55);
+    assert(0);
     assert(ptr->BPB_RootEntCnt == 0);
+    assert(0);
     assert(ptr->BPB_TotSec16 == 0);
+    assert(0);
     assert(ptr->BPB_NumFATs == 2 || ptr->BPB_NumFATs == 1);
+    assert(0);
 }
 
 void showFAT32HeadInfo(struct fat_header* pfatheader) {
