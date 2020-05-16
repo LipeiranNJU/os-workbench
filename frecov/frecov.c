4ab103eb1c48fcd5372432cf6191e0b59255149b
tracer-nju <tracer@nju.edu.cn>
2020-05-16 16:59:00 +0800

    Static hostname: lpr-PC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 65a278ae454e4feb90e03bd4754e808b            Boot ID: 5afb68b4831f4b0f99251ea80f18cbb7   Operating System: Deepin 20 Beta             Kernel: Linux 5.3.0-3-amd64       Architecture: x86-64  16:59:00 up 1 day, 17:38,  1 user,  load average: 3.14, 2.86, 2.99

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -200,11 +200,11 @@ int main(int argc, char *argv[]) {
             fwrite((void*) magicNum, 1, sizeof(*header), pfdpic);
             fclose(pfdpic);
             pfdpic = fopen(abspath, "a");
-            fwrite((void*) pBMInfoHeader, 1, pBMInfoHeader->biSize, pfdpic);
+            fwrite((void*) pBMInfoHeader, 1, pBMInfoHeader->biSize-14, pfdpic);
             fclose(pfdpic);
-            pfdpic = fopen(abspath, "a");
-            fwrite((void*) pBMInfoHeader, 1, header->bfSize - header->bfOffBits, pfdpic);
-            fclose(pfdpic);
+            // pfdpic = fopen(abspath, "a");
+            // fwrite((void*) pBMInfoHeader, 1, header->bfSize - header->bfOffBits, pfdpic);
+            // fclose(pfdpic);
             char buf[41] = {};
             buf[40] = 0;
             char cmd[100] = {};
