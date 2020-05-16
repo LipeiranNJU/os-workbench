446e4fb4b065e1f897a15de339700e070c08dbd9
tracer-nju <tracer@nju.edu.cn>
2020-05-16 11:37:21 +0800

    Static hostname: lpr-PC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 65a278ae454e4feb90e03bd4754e808b            Boot ID: 5afb68b4831f4b0f99251ea80f18cbb7   Operating System: Deepin 20 Beta             Kernel: Linux 5.3.0-3-amd64       Architecture: x86-64  11:37:21 up 1 day, 12:17,  1 user,  load average: 4.04, 4.49, 4.49

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -196,29 +196,29 @@ int main(int argc, char *argv[]) {
             // assert(0);
             fwrite((void*) magicNum, 1, header->bfSize, pfdpic);
             fclose(pfdpic);
-            // char buf[41] = {};
-            // buf[40] = 0;
-            // char cmd[100] = {};
-            // int pipefds[2];
-            // if(pipe(pipefds) < 0){
-		    //     perror("pipe");
-            //     assert(0);
-	        // }
-            // int pid = fork();
-            // char* argv[3];
-            // argv[0] = "sha1sum",
-            // argv[1] = abspath;
-            // argv[2] = NULL;
-            // if (pid == 0) {
-            //     close(pipefds[0]);
-            //     dup2(pipefds[1], fileno(stderr));
-            //     dup2(pipefds[1], fileno(stdout));
-            //     execvp("sha1sum", argv);
-            // } else {
-            //     close(pipefds[1]);
-            //     read(pipefds[0], buf, 40);
-            //     printf("%s    %s\n", buf, picName);
-            // }
+            char buf[41] = {};
+            buf[40] = 0;
+            char cmd[100] = {};
+            int pipefds[2];
+            if(pipe(pipefds) < 0){
+		        perror("pipe");
+                assert(0);
+	        }
+            int pid = fork();
+            char* argv[3];
+            argv[0] = "sha1sum",
+            argv[1] = abspath;
+            argv[2] = NULL;
+            if (pid == 0) {
+                close(pipefds[0]);
+                dup2(pipefds[1], fileno(stderr));
+                dup2(pipefds[1], fileno(stdout));
+                execvp("sha1sum", argv);
+            } else {
+                close(pipefds[1]);
+                read(pipefds[0], buf, 40);
+                printf("%s    %s\n", buf, picName);
+            }
 
         }
 
