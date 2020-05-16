7a9c872d596f35069cbb46fa2a1dac05c3eb741a
tracer-nju <tracer@nju.edu.cn>
2020-05-16 09:18:16 +0800

    Static hostname: lpr-PC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 65a278ae454e4feb90e03bd4754e808b            Boot ID: 5afb68b4831f4b0f99251ea80f18cbb7   Operating System: Deepin 20 Beta             Kernel: Linux 5.3.0-3-amd64       Architecture: x86-64  09:18:16 up 1 day,  9:57,  1 user,  load average: 0.83, 0.74, 0.54

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -139,7 +139,7 @@ int main(int argc, char *argv[]) {
         if (isFATShortDirectory(pFATdir) == true) {
             print("name:%s\n",pFATdir->DIR_Name);
             assert(pFATdir->DIR_FstClusHI == 0);
-            char* magicNum =(char *) (offset+(uintptr_t)(pfatheader)+(pFATdir->DIR_FstClusLO)*BPB_SecPerClus*BPB_BytsPerSec);
+            char* magicNum =(char *) (offset+(uintptr_t)(pfatheader)+(pFATdir->DIR_FstClusLO-2)*BPB_SecPerClus*BPB_BytsPerSec);
             print("%c%c\n",magicNum[0],magicNum[1]);
             assert(magicNum[0] == 'B');
             assert(magicNum[1] == 'M');
