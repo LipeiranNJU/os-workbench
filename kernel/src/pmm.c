3f67b255af111b72e3d501d869933df71531e804
tracer-nju <tracer@nju.edu.cn>
2020-05-21 14:10:33 +0800

    Static hostname: lpr-PC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 030b25f7d3ed4bf29e8db174e28df3cb            Boot ID: 918e6997889f41c9b472cc876e3a9f6b   Operating System: Deepin 20 Beta             Kernel: Linux 5.3.0-3-amd64       Architecture: x86-64  14:10:33 up 2 days,  1:57,  1 user,  load average: 0.70, 0.42, 0.32

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -1,14 +1,15 @@
 #include <common.h>
-
+void* tmp;
 static void *kalloc(size_t size) {
-
-  return NULL;
+  tmp = tmp+size;
+  return tmp;
 }
 
 static void kfree(void *ptr) {
 }
 
 static void pmm_init() {
+  tmp = _heap.start;
   uintptr_t pmsize = ((uintptr_t)_heap.end - (uintptr_t)_heap.start);
   printf("Got %d MiB heap: [%p, %p)\n", pmsize >> 20, _heap.start, _heap.end);
 }
