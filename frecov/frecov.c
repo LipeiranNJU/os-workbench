655e2e5710ca3b716797173d2271d5fd4f19a173
tracer-nju <tracer@nju.edu.cn>
2020-05-21 11:27:31 +0800

    Static hostname: lpr-PC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 030b25f7d3ed4bf29e8db174e28df3cb            Boot ID: 918e6997889f41c9b472cc876e3a9f6b   Operating System: Deepin 20 Beta             Kernel: Linux 5.3.0-3-amd64       Architecture: x86-64  11:27:31 up 1 day, 23:14,  1 user,  load average: 0.19, 0.24, 0.18

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -202,7 +202,7 @@ int main(int argc, char *argv[]) {
                     tmpl++;
             }
         } 
-        if (tmp>5 && tmpl>1.8*tmp) {
+        if (tmp>5 && tmpl>5) {
             dirClusAdd(i);     
         }
     }
