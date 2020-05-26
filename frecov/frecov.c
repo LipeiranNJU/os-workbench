7e2bb9bcd88ef3174f8cafc698d9f8301e592511
tracer-nju <tracer@nju.edu.cn>
2020-05-26 19:37:03 +0800

    Static hostname: lpr-PC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 030b25f7d3ed4bf29e8db174e28df3cb            Boot ID: 3bd7182f257543fba97b435ea12c33d5   Operating System: Deepin 20 Beta             Kernel: Linux 5.3.0-3-amd64       Architecture: x86-64  19:37:03 up  8:04,  1 user,  load average: 1.26, 1.32, 0.92

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -215,7 +215,7 @@ int main (int argc, char* argv[]) {
                     countl++;
             }
         }
-        if (countsh > 8 && countl > 1.5*countsh) {
+        if (countsh > 4 && countl > 1.5*countsh) {
             int index = getClusterIndex(cluster, imgDataStart, clusSize);
             // printf("%p\n", cluster);
             cluses[index] = DirEntry;
