580406dc478b7be2fe6c45d8880e571634a2efd7
tracer-nju <tracer@nju.edu.cn>
2020-05-27 00:35:44 +0800

    Static hostname: lpr-PC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 030b25f7d3ed4bf29e8db174e28df3cb            Boot ID: 3bd7182f257543fba97b435ea12c33d5   Operating System: Deepin 20 Beta             Kernel: Linux 5.3.0-3-amd64       Architecture: x86-64  00:35:44 up 13:03,  1 user,  load average: 2.44, 2.08, 1.86

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -257,8 +257,8 @@ bool isFATLongDirectory(const struct FATLongDirectory* pFATldir) {
 }
 char* readCompleteInfoFromFATShortDirectory(struct FATShortDirectory* pFATsd) {
     
-    char* name = malloc(sizeof(char) * 100);
-    memset(name, '\0', 100);
+    char* name = malloc(sizeof(char) * 200);
+    memset(name, '\0', 200);
     struct FATLongDirectory* pFATld = (struct FATLongDirectory*) (pFATsd-1);
     int i = -1;
     while((pFATld->LDIR_Ord >> 4)==0) {
