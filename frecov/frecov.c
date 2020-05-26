3e62cc99b1ef25f7e31e7070cc2053ca0d14af44
tracer-nju <tracer@nju.edu.cn>
2020-05-26 18:31:18 +0800

    Static hostname: lpr-PC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 030b25f7d3ed4bf29e8db174e28df3cb            Boot ID: 3bd7182f257543fba97b435ea12c33d5   Operating System: Deepin 20 Beta             Kernel: Linux 5.3.0-3-amd64       Architecture: x86-64  18:31:18 up  6:59,  1 user,  load average: 0.65, 0.34, 0.39

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -252,21 +252,6 @@ bool isFATLongDirectory(const struct FATLongDirectory* pFATldir) {
     if ((pFATldir->LDIR_Ord & 0x0f) > 3)
         return false;
     
-    for (int i = 0; i < 5; i++) {
-        if ((pFATldir->LDIR_Name1[i] >> 8) != 0)
-            return false;
-    }
-    
-    for (int i = 0; i < 6; i++) {
-        if ((pFATldir->LDIR_Name1[i] >> 8) != 0)
-            return false;
-    }
-    assert(0);
-    for (int i = 0; i < 2; i++) {
-        if ((pFATldir->LDIR_Name1[i] >> 8) != 0)
-            return false;
-    }
-    assert(0);
     if (pFATldir->LDIR_FstClusLO != 0 || pFATldir->LDIR_Type != 0)
         return false;
     
