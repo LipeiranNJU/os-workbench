4b71331887a549334814413a7972235682a18791
tracer-nju <tracer@nju.edu.cn>
2020-05-26 18:53:19 +0800

    Static hostname: lpr-PC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 030b25f7d3ed4bf29e8db174e28df3cb            Boot ID: 3bd7182f257543fba97b435ea12c33d5   Operating System: Deepin 20 Beta             Kernel: Linux 5.3.0-3-amd64       Architecture: x86-64  18:53:19 up  7:21,  1 user,  load average: 1.23, 0.71, 0.40

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -226,12 +226,13 @@ int main (int argc, char* argv[]) {
             printf("index:%d\n", i);
             void* cluster = getClusterFromIndex(i, imgDataStart);
             printf("%p\n", cluster);
+            int j = 0;
             for (struct FATShortDirectory* ptmp = cluster; inFile(ptmp, cluster, clusSize); ptmp++) {
                 if (isFATShortDirectory(ptmp)) {
                     char nameTmp[12];
                     memcpy(nameTmp, ptmp->DIR_Name, 11);
                     nameTmp[11] = '\0';
-                    printf("%s\n", nameTmp);
+                    printf("%s\t%d\n", nameTmp, ++j);
                 }
             }
         }
