035699df8aac138d0d107431eab7598de91c3c8e
tracer-nju <tracer@nju.edu.cn>
2020-05-26 12:24:48 +0800

    Static hostname: lpr-PC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 030b25f7d3ed4bf29e8db174e28df3cb            Boot ID: 3bd7182f257543fba97b435ea12c33d5   Operating System: Deepin 20 Beta             Kernel: Linux 5.3.0-3-amd64       Architecture: x86-64  12:24:49 up 52 min,  1 user,  load average: 1.93, 0.96, 0.57

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -196,7 +196,7 @@ int main (int argc, char* argv[]) {
     for (struct FATShortDirectory* ptmp = pFATshdir; inFile(ptmp, pFATshdir, imgDataSize); ptmp++) {
         if (ptmp->DIR_NTRes == 0 && (ptmp->DIR_Attr >> 6) == 0) {
             for (int i = 0; i < 11; i++) {
-                if (isprint(ptmp->DIR_Name[i]) || ptmp->DIR_Name[i] == '\0')
+                if (isprint(ptmp->DIR_Name[i]) || (i!=0 && ptmp->DIR_Name[i] == '\0'))
                     printf("%c", ptmp->DIR_Name[i]);
                 if (i == 10)
                     printf("\n");
