d6d7b437e9876fa9793c2f2d409c8fb28a51b188
tracer-nju <tracer@nju.edu.cn>
2020-05-26 21:10:57 +0800

    Static hostname: lpr-PC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 030b25f7d3ed4bf29e8db174e28df3cb            Boot ID: 3bd7182f257543fba97b435ea12c33d5   Operating System: Deepin 20 Beta             Kernel: Linux 5.3.0-3-amd64       Architecture: x86-64  21:10:58 up  9:38,  1 user,  load average: 2.50, 2.56, 2.34

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -13,8 +13,8 @@
 #include <ctype.h>
 #include <math.h>
 #define B 1
-//#define __DEBUG__
-//#define __DDEBUG__
+// #define __DEBUG__
+#define __DDEBUG__
 #define KB (1024 * B)
 #define MB (1024 * KB)
 #define GB (1024 * KB)
@@ -29,31 +29,16 @@
 #else
 #define printk(...) 
 #endif
-#define Unknown (-1)
-#define DirEntry 1
+#define beNotKown 0
+#define beBMPHeader 1
 #define beNotUsed 2
-#define BMPContent 3
+#define beBMPContent 3
 
-int BPB_BytsPerSec;
-int BPB_SecPerClus;
-int BPB_RootClus;
-int BPB_FATSz32;
-int BPB_HiddSec;
-int BPB_RsvdSecCnt;
-int BPB_NumFATs;
-int offset;
-int clusSize;
-inline bool inFile(const void* nowAddr, const void* fileStart, int fileSize) {
-    return ((intptr_t)(nowAddr) - (intptr_t)(fileStart)) < fileSize ? true : false;
-}
-inline int getClusterIndex(const void* addr, const void* start, int clusterSize) {
+inline int getClusterIndex(void* addr, void* start, int clusterSize) {
     return ((intptr_t)addr - (intptr_t) start) / clusterSize;
 }
-inline void* nextClus(const void* Clus) {
-    return (void*)((intptr_t)(Clus) + BPB_BytsPerSec * BPB_SecPerClus);
-}
 
-struct FATHeader {
+struct fat_header {
     uint8_t  BS_jmpBoot[3];
     uint8_t  BS_OEMName[8];
     uint16_t BPB_BytsPerSec;
@@ -139,123 +124,224 @@ struct FATLongDirectory {
     uint16_t LDIR_FstClusLO;
     uint16_t LDIR_Name3[2];
 }__attribute__((packed));
-bool isFATLongDirectory(const struct FATLongDirectory* pFATldir);
-bool isFATShortDirectory(const struct FATShortDirectory*);
-char* readCompleteInfoFromFATShortDirectory(struct FATShortDirectory* pFATsd);
-static inline struct FATLongDirectory* nextLongDirectory(struct FATLongDirectory* longDirectory){
-    return (struct FATLongDirectory*)((intptr_t)(longDirectory) + sizeof(struct FATLongDirectory));
-}
-static inline struct FATShortDirectory* nextShortDirectory(struct FATShortDirectory* shortDirectory){
-    return (struct FATShortDirectory*)((intptr_t)(shortDirectory) + sizeof(struct FATShortDirectory));
-}
-bool isValidFileName(char* name);
-int dirClus[100];
 
-void* getClusterFromIndex(int index_from_zero, void* initialClusterAddr) {
-    return initialClusterAddr+index_from_zero*clusSize;
-}
-void initAttr(struct FATHeader* pfatheader) {
-    BPB_BytsPerSec = pfatheader->BPB_BytsPerSec;
-    BPB_SecPerClus = pfatheader->BPB_SecPerClus;
-    BPB_RootClus = pfatheader->BPB_RootClus;
-    BPB_FATSz32 = pfatheader->BPB_FATSz32;
-    BPB_HiddSec = pfatheader->BPB_HiddSec;
-    BPB_RsvdSecCnt = pfatheader->BPB_RsvdSecCnt;
-    BPB_NumFATs = pfatheader->BPB_NumFATs;
-    clusSize = BPB_SecPerClus * BPB_BytsPerSec;
-}
-void dirClusAdd(int index) {
-    assert(index >= 0);
-    for (int i = 0; i < 100; i++) {
-        if (dirClus[i] == -1) {
-            dirClus[i] = index;
-            return;
-        }
-    }
-    assert(0);
-}
-int main (int argc, char* argv[]) {
-    char* imgName = argv[1];
+void lineCmp(uint8_t*, uint8_t*, uint8_t*, int);
+void verifyFAT32Head(struct fat_header*);
+void showFAT32HeadInfo(struct fat_header*);
+bool isFATShortDirectory(struct FATShortDirectory*);
+char* readInfoFromFATLongDirectory(struct FATLongDirectory* );
+
+int main(int argc, char *argv[]) {
+    assert(argc == 2);
+    assert(sizeof(struct fat_header) == 512);
+    assert(sizeof(struct FATShortDirectory) == 32);
+    assert(sizeof(struct FATLongDirectory) == 32);
+    char* fileName = argv[1];
+    print("Filename is %s\n", fileName);
+
+
     struct stat statbuf;
-    stat(imgName, &statbuf);
-    int imgSize = statbuf.st_size;
-    int imgFd = open(imgName, O_RDONLY, 0);
-    struct FATHeader* pFATHeader = (struct FATHeader *) mmap(NULL, imgSize, PROT_READ, MAP_SHARED, imgFd, 0);
+    stat(fileName,&statbuf);
+    int size = statbuf.st_size;
+    print("img file size is %d\n", size);
 
-    BPB_BytsPerSec = pFATHeader->BPB_BytsPerSec;
-    BPB_SecPerClus = pFATHeader->BPB_SecPerClus;
-    BPB_RootClus = pFATHeader->BPB_RootClus;
-    BPB_FATSz32 = pFATHeader->BPB_FATSz32;
-    BPB_HiddSec = pFATHeader->BPB_HiddSec;
-    BPB_RsvdSecCnt = pFATHeader->BPB_RsvdSecCnt;
-    BPB_NumFATs = pFATHeader->BPB_NumFATs;
-    clusSize = BPB_BytsPerSec * BPB_SecPerClus;
-    int imgOffset = (BPB_RsvdSecCnt+BPB_NumFATs*BPB_FATSz32+(BPB_RootClus-2)*BPB_SecPerClus+BPB_HiddSec)*BPB_BytsPerSec;
-    struct FATShortDirectory* pFATshdir = (void* )pFATHeader + imgOffset;
-    void* imgDataStart = (void* )pFATHeader + imgOffset;
-    int imgDataSize = imgSize - imgOffset;
-    int tmpi = 0;
-    int clusNum = imgDataSize / clusSize;
-    int* cluses = malloc(sizeof(int)*clusNum);
-    for (int i = 0; i < clusNum; i++) 
-        cluses[i] = Unknown;
-    for (void* cluster = imgDataStart; inFile(cluster, imgDataStart, imgDataSize); cluster = nextClus(cluster)) {
-        int countsh = 0;
-        int countl = 0;
-        for (struct FATShortDirectory* ptmp = cluster; inFile(ptmp, cluster, clusSize); ptmp++) {
-            if (isFATShortDirectory(ptmp)) {
-                char nameTmp[12];
-                memcpy(nameTmp, ptmp->DIR_Name, 11);
-                nameTmp[11] = '\0';
-                tmpi++;
-                countsh++;
-                if (isFATLongDirectory((void*)(ptmp-1)))
-                    countl++;
-                if (isFATLongDirectory((void*)(ptmp-2)))
-                    countl++;
+    int fd = open(fileName, O_RDONLY, 0);
+    assert(fd > 0);
+    print("fd is %d\n", fd);
+    struct fat_header* pfatheader =(struct fat_header*) mmap(NULL, size, PROT_READ, MAP_SHARED , fd, 0);
+    print("SizoOf FATheader is %d\n",(int) sizeof(struct fat_header));
+    verifyFAT32Head(pfatheader);
+    assert(pfatheader != NULL);
+    showFAT32HeadInfo(pfatheader);
+
+    int BPB_BytsPerSec = pfatheader->BPB_BytsPerSec;
+    int BPB_SecPerClus = pfatheader->BPB_SecPerClus;
+    int BPB_RootClus = pfatheader->BPB_RootClus;
+    int BPB_FATSz32 = pfatheader->BPB_FATSz32;
+    int BPB_HiddSec =pfatheader->BPB_HiddSec;
+    int BPB_RsvdSecCnt = pfatheader->BPB_RsvdSecCnt;
+    int BPB_NumFATs = pfatheader->BPB_NumFATs;
+    
+    int offset = (BPB_RsvdSecCnt + BPB_NumFATs * BPB_FATSz32 + (BPB_RootClus - 2) * BPB_SecPerClus + BPB_HiddSec) * BPB_BytsPerSec;
+    print("Offset of initial clus is %d\n", offset);
+    struct FATShortDirectory* pFATdir = (struct FATShortDirectory*)((intptr_t)pfatheader+offset);
+    void* fatContentStart = (void*)((intptr_t)pfatheader+offset);
+    int canBeUsed = 0;
+    bool skip = false;
+    print("Total Sec is %d\n", (int) pfatheader->BPB_TotSec32);
+    for (; (intptr_t)(pFATdir) < (intptr_t)(pfatheader)+size;pFATdir++) {
+        assert((intptr_t)pFATdir-(intptr_t)pfatheader < pfatheader->BPB_TotSec32*pfatheader->BPB_BytsPerSec);
+        if (isFATShortDirectory(pFATdir) == true) {
+            // printf("%lX\n", (long) (intptr_t)pFATdir-((intptr_t)pfatheader+offset));
+            print("name:%s\n",pFATdir->DIR_Name);
+            assert(pFATdir->DIR_FstClusHI == 0);
+            char* magicNum = (char *) (offset + (uintptr_t)(pfatheader) + (pFATdir->DIR_FstClusLO - BPB_RootClus) * BPB_SecPerClus * BPB_BytsPerSec);
+            assert(magicNum[0] == 'B');
+            assert(magicNum[1] == 'M');
+            struct BMPHeader* header = (struct BMPHeader*) magicNum;
+            assert(header->bfReserved1 == 0);
+            assert(header->bfReserved2 == 0);
+            print("Size:%d\n",header->bfSize);
+            print("Offbits:%d\n", header->bfOffBits);
+            struct BMPInfoHeader* pBMInfoHeader = (struct BMPInfoHeader*) (header + 1);
+            assert(pBMInfoHeader->biHeight>0);
+            assert(pBMInfoHeader->biSize == 40);
+            if (pBMInfoHeader->biCompression == 0) {
+                print("will not be compressed.\n");
+            } else {
+                print("will be compressed\n");
+                assert(0);
             }
-        }
-        if (countsh > 6 && countl >= countsh) {
-            int index = getClusterIndex(cluster, imgDataStart, clusSize);
-            // printf("%p\n", cluster);
-            cluses[index] = DirEntry;
-        }
-    }
-    for (int i = 0; i < clusNum; i++) {
-        if (cluses[i] == DirEntry) {
-            // printf("index:%d\n", i);
-            void* cluster = getClusterFromIndex(i, imgDataStart);
-            // printf("%p\n", cluster);
-            int j = 0;
-            for (struct FATShortDirectory* ptmp = cluster; inFile(ptmp, cluster, clusSize); ptmp++) {
-                if (isFATShortDirectory(ptmp) && isFATLongDirectory((void*)(ptmp-1))) {
-                    char nameTmp[12];
-                    memcpy(nameTmp, ptmp->DIR_Name, 11);
-                    nameTmp[11] = '\0';
-                    // printf("%s\t%d\n", nameTmp, ++j);
-                    readCompleteInfoFromFATShortDirectory(ptmp);
+
+            canBeUsed += 1;
+            struct FATLongDirectory* pFATld = (struct FATLongDirectory*)(pFATdir - 1);
+            char * picName = readInfoFromFATLongDirectory(pFATld);
+            assert(picName != NULL);
+            char* prefix = "/tmp/";
+            int size = strlen(prefix) + strlen(picName);
+            char* abspath = malloc(sizeof(char) * (size + 1));
+            memset(abspath, '\0', size + 1);
+            strcat(strcat(abspath, prefix), picName);
+            // free(picName);
+            print("PicStoredPath:%s\n", abspath);
+            // assert(0);
+            skip = false;
+            for (int i = 0; i < strlen(abspath); i++) {
+                if (!isalnum(abspath[i]) && abspath[i] != '.' && abspath[i] != '/') {
+                    skip = true;
+                    // printf("invalid name:%s\n",abspath);
+                    break;
                 }
             }
+            if (skip) {
+                continue;
+            }
+            FILE* pfdpic = fopen(abspath, "w+");
+            // assert(0);
+            fwrite((void*) magicNum, 1, sizeof(*header), pfdpic);
+            fclose(pfdpic);
+            pfdpic = fopen(abspath, "a");
+            fwrite((void*) pBMInfoHeader, 1, pBMInfoHeader->biSize, pfdpic);
+            fclose(pfdpic);
+            int lineWidthSize = pBMInfoHeader->biWidth*4;
+            pfdpic = fopen(abspath, "a");
+            // 位图数据区写入
+            int picDataSize = header->bfSize - header->bfOffBits;
+            void* picData = malloc(picDataSize);
+            uint8_t* preLine = malloc(picDataSize);
+            uint8_t* nowLine = malloc(picDataSize);
+            uint8_t* laterLine = malloc(picDataSize);
+            // printk("cluster index is%d\n", getClusterIndex(pFATdir, fatContentStart, 4*KB));
+            void* picDataStart = (void*) ((uintptr_t)(header) + header->bfOffBits);
+            // bool tempflag = true;
+            fwrite(picDataStart, 1, picDataSize/*(i+1)*lineWidthSize*/, pfdpic);
+            fclose(pfdpic);
+            free(picData);
+            free(preLine);
+            free(nowLine);
+            if (((intptr_t) pFATdir - (intptr_t)pfatheader -offset) % (4*KB) != 0) {
+                
+            }
+            char buf[41] = {};
+            buf[40] = 0;
+            char cmd[100] = {};
+            int pipefds[2];
+            if(pipe(pipefds) < 0){
+		        perror("pipe");
+                assert(0);
+	        }
+            int pid = fork();
+            char* argv[3];
+            argv[0] = "sha1sum",
+            argv[1] = abspath;
+            argv[2] = NULL;
+            if (pid == 0) {
+                close(pipefds[0]);
+                dup2(pipefds[1], fileno(stderr));
+                dup2(pipefds[1], fileno(stdout));
+                execvp("sha1sum", argv);
+            } else {
+                close(pipefds[1]);
+                read(pipefds[0], buf, 40);
+                printf("%s    %s\n", buf, picName);
+            }
+
         }
+
+        assert((intptr_t) (pFATdir + 1) - (intptr_t)pFATdir == sizeof(struct FATShortDirectory));
+        assert(pFATdir != NULL);
+        fflush(stdout);
     }
-    return 0;
+    print("%d can be short name directory.\n",canBeUsed);
+    close(fd);
+    return 0;    
 }
 
-bool isFATShortDirectory(const struct FATShortDirectory* ptmp) {
-    if (strncmp((char*)&ptmp->DIR_Name[8], "BMP", 3) == 0) {
-        return true;
-    }
-    return false;
+void verifyFAT32Head(struct fat_header* ptr) {
+    print("hello\n");
+    assert((memcmp(&ptr->BS_FilSysType0, "FAT32", 5) == 0));
+    assert(ptr->Signature_word == 0xAA55);
+    assert(ptr->BPB_RootEntCnt == 0);
+    assert(ptr->BPB_TotSec16 == 0);
+    assert(ptr->BPB_NumFATs == 2 || ptr->BPB_NumFATs == 1);
 }
 
-bool isFATLongDirectory(const struct FATLongDirectory* pFATldir) {
+void showFAT32HeadInfo(struct fat_header* pfatheader) {
+    print("SizoOf FATheader is %d\n",(int) sizeof(struct fat_header));
+    print("jmpBoot[0] is %X\t", pfatheader->BS_jmpBoot[0]);
+    print("jmpBoot[2] is %X\n", pfatheader->BS_jmpBoot[2]);
+    print("BPB_BytsPerSec is %d\tBPB_SecPerClus is %d\n", pfatheader->BPB_BytsPerSec, pfatheader->BPB_SecPerClus); 
+    print("BPB_RootClus is %d\n", pfatheader->BPB_RootClus);
+    print("BPB_FATSz32 is %d\n", pfatheader->BPB_FATSz32);
+    print("BPB_HiddSec is %d\n", pfatheader->BPB_HiddSec);
+    print("BPB_RsvdSecCnt is %d\n", pfatheader->BPB_RsvdSecCnt);
+    print("BPB_NumFATs is %d\n", pfatheader->BPB_NumFATs);
+}
 
-    if (pFATldir->LDIR_FstClusLO != 0 || pFATldir->LDIR_Type != 0)
+bool isLegalInShort(char c) {
+    if (c == 0x22 || c==0x2A || c==0x2B||c==0x2C ||c== 0x2E||c==0x2F ||c== 0x3A||c== 0x3B||c==0x3C ||c== 0x3D||c==0x3E ||c ==0x3F||c==0x5B||c==0x5C||c==0x5D||c==0x7C)
         return false;
-    else    
+    else
         return true;
+    
 }
+
+bool isFATShortDirectory(struct FATShortDirectory* pFATdir) {
+    // if (pFATdir->DIR_CrtTime % 2 == 1)
+    //     return false;
+    // else if (pFATdir->DIR_CrtTimeTenth > 199 || pFATdir->DIR_CrtTimeTenth < 0)
+    //     return false;
+    // else if (pFATdir->DIR_NTRes != 0)
+    //     return false;
+    // else if (pFATdir->DIR_FileSize > 2 * MB)
+    //     return false;
+    // else if (pFATdir->DIR_CrtDate > pFATdir->DIR_LstAccDate || pFATdir->DIR_CrtDate > pFATdir->DIR_WrtDate || pFATdir->DIR_LstAccDate > pFATdir->DIR_WrtDate)
+    //     return false;
+    // else {
+        // int year = ((pFATdir->DIR_CrtDate & 0xfd00) >> 9);
+        // int month = ((pFATdir->DIR_CrtDate & 0xf0) >> 4); 
+        // int day = (pFATdir->DIR_CrtDate & 0xf);
+        // if (year < 5 || year > 40)
+        //     return false;
+        // if (month < 1 || month > 12)
+        //     return false;
+        // if (day == 0)
+        //     return false;
+        
+        // for (int i = 0; i < 11; i++) {
+        //     if ((i != 0 && pFATdir->DIR_Name[i] < 0x20) || (i == 0 && pFATdir->DIR_Name[i] ==0x05) || (!isLegalInShort(pFATdir->DIR_Name[i])))
+        //         return false;
+        // }
+        if (strncmp((char *)&pFATdir->DIR_Name[8], "BMP", 3) == 0 && isalnum(pFATdir->DIR_Name[0]))
+            return true;
+
+        return false;
+    // }
+    
+}
+
 char* readCompleteInfoFromFATShortDirectory(struct FATShortDirectory* pFATsd) {
+    
     char* name = malloc(sizeof(char) * 100);
     memset(name, '\0', 100);
     struct FATLongDirectory* pFATld = (struct FATLongDirectory*) (pFATsd-1);
@@ -295,10 +381,51 @@ char* readCompleteInfoFromFATShortDirectory(struct FATShortDirectory* pFATsd) {
         if (!isprint(name[i]))
             return NULL;
     }
-    for (int i = 0; i < 40; i++)
-        printf("a");
-    printf("   ");
-    fflush(stdout);
-    printf("%s\n", name);
-    return NULL;
+    return name;
+}
+int cmpfunc (const void * a, const void * b)
+{
+   return ( *(double*)a - *(double*)b );
+}
+void lineCmp(uint8_t* preLine, uint8_t* nowLine,uint8_t* latterLine, int size) {
+    int width = size/4;
+    int sum[3] = {0};
+    int Gx_B = 0;
+    int Gy_B = 0;
+    int G_B = 0;
+    int Gx_G = 0;
+    int Gy_G = 0;
+    int G_G = 0;
+    int Gx_R = 0;
+    int Gy_R = 0;
+    int G_R = 0;
+    double* list = malloc(sizeof(double)*(width-1));
+    double linesum = 0;
+    for (int i = 1; i < width-1; i++){
+        Gx_B = 2*nowLine[(i+1)*4+0] - 2*nowLine[(i-1)*4+0] + preLine[(i+1)*4+0] - preLine[(i-1)*4+0] + latterLine[(i+1)*4+0] - latterLine[(i-1)*4+0];
+        Gy_B = 2*latterLine[(i)*4+0] - 2*preLine[(i)*4+0] - preLine[(i+1)*4+0] - preLine[(i-1)*4+0] + latterLine[(i+1)*4+0] + latterLine[(i-1)*4+0];
+        G_B = sqrt(1.0*Gx_B*Gx_B+1.0*Gy_B*Gy_B);
+        sum[0]+=G_B;
+        
+
+        Gx_G = 2*nowLine[(i+1)*4+1] - 2*nowLine[(i-1)*4+1] + preLine[(i+1)*4+1] - preLine[(i-1)*4+1] + latterLine[(i+1)*4+1] - latterLine[(i-1)*4+1];
+        Gy_G = 2*latterLine[(i)*4+1] - 2*preLine[(i)*4+1] - preLine[(i+1)*4+1] - preLine[(i-1)*4+1] + latterLine[(i+1)*4+1] + latterLine[(i-1)*4+1];
+        G_G = sqrt(1.0*Gx_G*Gx_G+1.0*Gy_G*Gy_G);
+        sum[1]+=G_G;
+
+        
+        Gx_R = 2*nowLine[(i+1)*4+2] - 2*nowLine[(i-1)*4+2] + preLine[(i+1)*4+2] - preLine[(i-1)*4+2] + latterLine[(i+1)*4+2] - latterLine[(i-1)*4+2];
+        Gy_R = 2*latterLine[(i)*4+2] - 2*preLine[(i)*4+2] - preLine[(i+1)*4+2] - preLine[(i-1)*4+2] + latterLine[(i+1)*4+2] + latterLine[(i-1)*4+2];
+        G_R = sqrt(1.0*Gx_R*Gx_R+1.0*Gy_R*Gy_R);
+        sum[2]+=G_R;
+        list[i-1] = sqrt(1.0*sum[0]*sum[0]+1.0*sum[1]*sum[1]+1.0*sum[2]*sum[2]);
+        double length = sqrt(1.0*sum[0]*sum[0]+1.0*sum[1]*sum[1]+1.0*sum[2]*sum[2]);
+        linesum += length;
+    }
+    qsort(list, width-1,sizeof(double),cmpfunc);
+    printf("\n");
+    for (int i = 0; i < width-1;i++)
+        printf("%lf ",list[i]);
+
+    printf("\n%lf\n", linesum/(width-2));
 }
