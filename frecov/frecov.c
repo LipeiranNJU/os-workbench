23eb0f37897b1a6feb7c261a6296acf3d59cc8f6
tracer-nju <tracer@nju.edu.cn>
2020-05-26 17:48:26 +0800

    Static hostname: lpr-PC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 030b25f7d3ed4bf29e8db174e28df3cb            Boot ID: 3bd7182f257543fba97b435ea12c33d5   Operating System: Deepin 20 Beta             Kernel: Linux 5.3.0-3-amd64       Architecture: x86-64  17:48:26 up  6:16,  1 user,  load average: 0.73, 0.42, 0.32

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -220,6 +220,16 @@ int main (int argc, char* argv[]) {
     for (int i = 0; i < clusNum; i++) {
         if (cluses[i] > 0) {
             printf("index:%d\n", i);
+            void* cluster = getClusterFromIndex(i, imgDataStart);
+            for (struct FATShortDirectory* ptmp = cluster; inFile(ptmp, cluster, clusSize); ptmp++) {
+                if (ptmp->DIR_NTRes == 0 && (ptmp->DIR_Attr >> 6) == 0 && ptmp->DIR_FstClusHI == 0) {
+                    if (strncmp((char*)&ptmp->DIR_Name[8], "BMP", 3) == 0) {
+                        char nameTmp[12];
+                        memcpy(nameTmp, ptmp->DIR_Name, 11);
+                        nameTmp[11] = '\0';
+                    }
+                }
+            }
         }
     }
     return 0;
