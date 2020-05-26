87ce0be91dd02d7bdd7731888bae476b2fd1d38f
tracer-nju <tracer@nju.edu.cn>
2020-05-26 19:30:11 +0800

    Static hostname: lpr-PC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 030b25f7d3ed4bf29e8db174e28df3cb            Boot ID: 3bd7182f257543fba97b435ea12c33d5   Operating System: Deepin 20 Beta             Kernel: Linux 5.3.0-3-amd64       Architecture: x86-64  19:30:11 up  7:57,  1 user,  load average: 0.64, 0.62, 0.52

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -291,6 +291,20 @@ char* readCompleteInfoFromFATShortDirectory(struct FATShortDirectory* pFATsd) {
         name[i*13+12] = (char) pFATld->LDIR_Name3[1];
         pFATld = pFATld-1;
     }
+    i += 1;
+    name[i*13+0] = (char) pFATld->LDIR_Name1[0];
+    name[i*13+1] = (char) pFATld->LDIR_Name1[1];
+    name[i*13+2] = (char) pFATld->LDIR_Name1[2];
+    name[i*13+3] = (char) pFATld->LDIR_Name1[3];
+    name[i*13+4] = (char) pFATld->LDIR_Name1[4];
+    name[i*13+5] = (char) pFATld->LDIR_Name2[0];
+    name[i*13+6] = (char) pFATld->LDIR_Name2[1];
+    name[i*13+7] = (char) pFATld->LDIR_Name2[2];
+    name[i*13+8] = (char) pFATld->LDIR_Name2[3];
+    name[i*13+9] = (char) pFATld->LDIR_Name2[4];
+    name[i*13+10] = (char) pFATld->LDIR_Name2[5];
+    name[i*13+11] = (char) pFATld->LDIR_Name3[0];
+    name[i*13+12] = (char) pFATld->LDIR_Name3[1];
     printf("%s\n", name);
     return NULL;
 }
