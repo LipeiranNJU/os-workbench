c550f6f9793940d31f8cc73804746d3ac9afb0f3
tracer-nju <tracer@nju.edu.cn>
2020-05-27 22:10:23 +0800

    Static hostname: lpr-PC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 030b25f7d3ed4bf29e8db174e28df3cb            Boot ID: 9669e9f78d3b427e9c6cc16f941c0dd4   Operating System: Deepin 20 Beta             Kernel: Linux 5.3.0-3-amd64       Architecture: x86-64  22:10:23 up  4:18,  1 user,  load average: 3.84, 3.76, 3.42

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -276,8 +276,8 @@ int main (int argc, char* argv[]) {
                                         for (int j = 0; j < realWidthSize/ByteperPixel-2; j++)
                                             printf("%lf\t", g[j]);
                                         printf("\n");
-                                        if (g[realWidthSize/ByteperPixel-10]>400)
-                                            blank = false;
+                                        if (g[0]>200)
+                                            blank = true;
                                         else
                                             blank = false;
                                     }
