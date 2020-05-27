20e73ca712baeda8ff1c00dbdc60522ecb7eb196
tracer-nju <tracer@nju.edu.cn>
2020-05-28 00:13:26 +0800

    Static hostname: lpr-PC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 030b25f7d3ed4bf29e8db174e28df3cb            Boot ID: 5740db00f2b5405484a828751b662695   Operating System: Deepin 20 Beta             Kernel: Linux 5.3.0-3-amd64       Architecture: x86-64  00:13:26 up 48 min,  1 user,  load average: 3.31, 3.77, 3.67

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -274,23 +274,24 @@ int main (int argc, char* argv[]) {
                             memcpy(nowline, source+i*realWidthSize, realWidthSize);
                             if (i != picHeight-1 && i!= 0) {
                                 memcpy(higherline, source+(i+1)*realWidthSize, realWidthSize);
-                                if (strcmp(name, "0M15CwG1yP32UPCp.bmp") == 0 || strcmp(name, "WDESkd1ohYoeScb0.bmp") == 0|| strcmp(name, "rgwfuyGZAfPrLw6n.bmp") == 0) {
+                                if (strcmp(name, "rgwfuyGZAfPrLw6n.bmp") == 0) {
                                     if (getClusterIndex(source+i*realWidthSize, imgDataStart, clusSize) != getClusterIndex(source+(i-1)*realWidthSize, imgDataStart, clusSize)) {
                                         double* g = sobelY(lowerline, nowline, higherline, realWidthSize/ByteperPixel);
                                         if (*g>200) {
                                             double tmpLow = 100000;
                                             int tmpLowIndex = -1;
-                                            // for (int i = 0; i < clusNum; i++) {
-                                            //     void* tmpcluster = getClusterFromIndex(i, imgDataStart);
-                                            //     memcpy(tmpnowline, tmpcluster, realWidthSize);
-                                            //     memcpy(tmphigherline, tmpcluster+realWidthSize, realWidthSize);
-                                            //     double* tmpd = sobelY(lowerline,tmpnowline, tmphigherline, realWidthSize/ByteperPixel);
-                                            //     if (*tmpd < tmpLow) {
-                                            //         tmpLow = *tmpd;
-                                            //         tmpLowIndex = i;
-                                            //     }
-                                            // // assert(*tmpd >= *g);
-                                            // }
+                                            for (int i = 0; i < clusNum; i++) {
+                                                void* tmpcluster = getClusterFromIndex(i, imgDataStart);
+                                                memcpy(tmpnowline, tmpcluster, realWidthSize);
+                                                memcpy(tmphigherline, tmpcluster+realWidthSize, realWidthSize);
+                                                double* tmpd = sobelY(lowerline,tmpnowline, tmphigherline, realWidthSize/ByteperPixel);
+                                                if (*tmpd < tmpLow) {
+                                                    tmpLow = *tmpd;
+                                                    tmpLowIndex = i;
+                                                }
+                                            // assert(*tmpd >= *g);
+                                            }
+                                            assert(0);
                                             void* newCluster = getClusterFromIndex(tmpLowIndex, imgDataStart);
                                             // source = newCluster - i*realWidthSize;
                                             memcpy(nowline, source+i*realWidthSize, realWidthSize);
