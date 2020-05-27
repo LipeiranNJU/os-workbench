e455e136b389a0a78b2a5d59b0323b488826be75
tracer-nju <tracer@nju.edu.cn>
2020-05-27 23:28:58 +0800

    Static hostname: lpr-PC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 030b25f7d3ed4bf29e8db174e28df3cb            Boot ID: 5740db00f2b5405484a828751b662695   Operating System: Deepin 20 Beta             Kernel: Linux 5.3.0-3-amd64       Architecture: x86-64  23:28:58 up 4 min,  1 user,  load average: 0.99, 1.15, 0.54

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -275,18 +275,18 @@ int main (int argc, char* argv[]) {
                                 if (strcmp(name, "0M15CwG1yP32UPCp.bmp") == 0 || strcmp(name, "WDESkd1ohYoeScb0.bmp") == 0) {
                                     if (getClusterIndex(picData+i*realWidthSize, imgDataStart, clusSize) != getClusterIndex(picData+(i-1)*realWidthSize, imgDataStart, clusSize)) {
                                         double* g = sobelY(lowerline, nowline, higherline, realWidthSize/ByteperPixel);
-                                        if (*g>300){
-                                            for (int i = 0; i < clusNum; i++) {
-                                                void* tmpcluster = getClusterFromIndex(i, imgDataStart);
-                                                memcpy(tmpnowline, tmpcluster, realWidthSize);
-                                                memcpy(tmphigherline, tmpcluster+realWidthSize, realWidthSize);
-                                                double* tmpd = sobelY(lowerline,tmpnowline, tmphigherline, realWidthSize/ByteperPixel);
-                                            assert(*tmpd >= *g);
-                                            }
-                                        }
-                                        for (int j = 0; j < realWidthSize/ByteperPixel-2; j++)
-                                            printf("%lf\t", g[j]);
-                                        printf("\n");
+                                        // if (*g>300) {
+                                        //     for (int i = 0; i < clusNum; i++) {
+                                        //         void* tmpcluster = getClusterFromIndex(i, imgDataStart);
+                                        //         memcpy(tmpnowline, tmpcluster, realWidthSize);
+                                        //         memcpy(tmphigherline, tmpcluster+realWidthSize, realWidthSize);
+                                        //         double* tmpd = sobelY(lowerline,tmpnowline, tmphigherline, realWidthSize/ByteperPixel);
+                                        //     assert(*tmpd >= *g);
+                                        //     }
+                                        // }
+                                        // for (int j = 0; j < realWidthSize/ByteperPixel-2; j++)
+                                        //     printf("%lf\t", g[j]);
+                                        // printf("\n");
                                         if (g[0]>190)
                                             blank = false;
                                         else
