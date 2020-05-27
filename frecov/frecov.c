9a0d3086d17043c8ff95452148df0797fe7bcc54
tracer-nju <tracer@nju.edu.cn>
2020-05-28 01:27:34 +0800

    Static hostname: lpr-PC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 030b25f7d3ed4bf29e8db174e28df3cb            Boot ID: 5f44185b852143ea9cea7f6212c937b5   Operating System: Deepin 20 Beta             Kernel: Linux 5.3.0-3-amd64       Architecture: x86-64  01:27:34 up 5 min,  1 user,  load average: 4.29, 2.30, 1.03

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -281,25 +281,25 @@ int main (int argc, char* argv[]) {
                                             int countUpper = 0;
                                             double tmpLow = *g;
                                             int tmpLowIndex = -1;
-                                            // for (int i = 0; i < clusNum; i++) {
-                                            //     void* tmpcluster = getClusterFromIndex(i, imgDataStart);
-                                            //     memcpy(tmpnowline, tmpcluster, realWidthSize);
-                                            //     memcpy(tmphigherline, tmpcluster+realWidthSize, realWidthSize);
-                                            //     double* tmpd = sobelY(lowerline,tmpnowline, tmphigherline, realWidthSize/ByteperPixel);
-                                            //     if (*tmpd < tmpLow && *tmpd > 1) {
-                                            //         tmpLow = *tmpd;
-                                            //         tmpLowIndex = i;
-                                            //         countUpper++;
-                                            //     }
-                                            //     // assert(*tmpd >= *g);
-                                            // }
-                                            // if (countUpper > 5) {
-                                            //     printf("CountUpper%d\n", countUpper);
-                                            //     void* newCluster = getClusterFromIndex(tmpLowIndex, imgDataStart);
-                                            //     source = newCluster - i*realWidthSize;
-                                            //     *g = tmpLow;
-                                            //     memcpy(nowline, source+i*realWidthSize, realWidthSize);
-                                            // }
+                                            for (int i = 0; i < clusNum; i++) {
+                                                void* tmpcluster = getClusterFromIndex(i, imgDataStart);
+                                                memcpy(tmpnowline, tmpcluster, realWidthSize);
+                                                memcpy(tmphigherline, tmpcluster+realWidthSize, realWidthSize);
+                                                double* tmpd = sobelY(lowerline,tmpnowline, tmphigherline, realWidthSize/ByteperPixel);
+                                                if (*tmpd < tmpLow && *tmpd > 1) {
+                                                    tmpLow = *tmpd;
+                                                    tmpLowIndex = i;
+                                                    countUpper++;
+                                                }
+                                                // assert(*tmpd >= *g);
+                                            }
+                                            if (countUpper > 5) {
+                                                printf("CountUpper%d\n", countUpper);
+                                                void* newCluster = getClusterFromIndex(tmpLowIndex, imgDataStart);
+                                                source = newCluster - i*realWidthSize;
+                                                *g = tmpLow;
+                                                memcpy(nowline, source+i*realWidthSize, realWidthSize);
+                                            }
                                             
                                         }
                                         printf("mean:%lf\n", *g);
