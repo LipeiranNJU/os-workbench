9b60f67bb331d551c0344f2bac055e9349b7af38
tracer-nju <tracer@nju.edu.cn>
2020-05-27 23:57:37 +0800

    Static hostname: lpr-PC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 030b25f7d3ed4bf29e8db174e28df3cb            Boot ID: 5740db00f2b5405484a828751b662695   Operating System: Deepin 20 Beta             Kernel: Linux 5.3.0-3-amd64       Architecture: x86-64  23:57:38 up 32 min,  1 user,  load average: 4.27, 3.93, 3.20

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -268,48 +268,47 @@ int main (int argc, char* argv[]) {
                         uint8_t* tmpnowline = malloc(realWidthSize);
                         uint8_t* tmphigherline = malloc(realWidthSize);
                         bool blank = false;
-                        // void* source = picData;
+                        // void* source =
                         for (int i = 0; i < picHeight; i++) {
                             memcpy(nowline, picData+i*realWidthSize, realWidthSize);
                             if (i != picHeight-1 && i!= 0) {
                                 memcpy(higherline, picData+(i+1)*realWidthSize, realWidthSize);
-                                // if (strcmp(name, "0M15CwG1yP32UPCp.bmp") == 0 || strcmp(name, "WDESkd1ohYoeScb0.bmp") == 0|| strcmp(name, "rgwfuyGZAfPrLw6n.bmp") == 0) {
-                                //     if (getClusterIndex(source+i*realWidthSize, imgDataStart, clusSize) != getClusterIndex(source+(i-1)*realWidthSize, imgDataStart, clusSize)) {
-                                //         double* g = sobelY(lowerline, nowline, higherline, realWidthSize/ByteperPixel);
-                                //         if (*g>200) {
-                                //             double tmpLow = 100000;
-                                //             int tmpLowIndex = -1;
-                                //             for (int i = 0; i < clusNum; i++) {
-                                //                 void* tmpcluster = getClusterFromIndex(i, imgDataStart);
-                                //                 memcpy(tmpnowline, tmpcluster, realWidthSize);
-                                //                 memcpy(tmphigherline, tmpcluster+realWidthSize, realWidthSize);
-                                //                 double* tmpd = sobelY(lowerline,tmpnowline, tmphigherline, realWidthSize/ByteperPixel);
-                                //                 if (*tmpd < tmpLow) {
-                                //                     tmpLow = *tmpd;
-                                //                     tmpLowIndex = i;
-                                //                 }
-                                //             // assert(*tmpd >= *g);
-                                //             assert(tmpLowIndex > -10);
-                                //             }
-
+                                if (strcmp(name, "0M15CwG1yP32UPCp.bmp") == 0 || strcmp(name, "WDESkd1ohYoeScb0.bmp") == 0|| strcmp(name, "rgwfuyGZAfPrLw6n.bmp") == 0) {
+                                    if (getClusterIndex(picData+i*realWidthSize, imgDataStart, clusSize) != getClusterIndex(picData+(i-1)*realWidthSize, imgDataStart, clusSize)) {
+                                        double* g = sobelY(lowerline, nowline, higherline, realWidthSize/ByteperPixel);
+                                        if (*g>200) {
+                                            double tmpLow = 100000;
+                                            int tmpLowIndex = -1;
+                                            for (int i = 0; i < clusNum; i++) {
+                                                void* tmpcluster = getClusterFromIndex(i, imgDataStart);
+                                                memcpy(tmpnowline, tmpcluster, realWidthSize);
+                                                memcpy(tmphigherline, tmpcluster+realWidthSize, realWidthSize);
+                                                double* tmpd = sobelY(lowerline,tmpnowline, tmphigherline, realWidthSize/ByteperPixel);
+                                                if (*tmpd < tmpLow) {
+                                                    tmpLow = *tmpd;
+                                                    tmpLowIndex = i;
+                                                }
+                                            // assert(*tmpd >= *g);
+                                            assert(tmpLowIndex > -10);
+                                            }
                                             
-                                //         }
-                                //         // printf("mean:%lf\n", *g);
-                                //         // for (int j = 0; j < realWidthSize/ByteperPixel-2; j++)
-                                //         //     printf("%lf\t", g[j]);
-                                //         // printf("\n");
-                                //         if (g[0]>190)
-                                //             blank = false;
-                                //         else
-                                //             blank = false;
-                                //     }
-                                // }
+                                        }
+                                        // printf("mean:%lf\n", *g);
+                                        // for (int j = 0; j < realWidthSize/ByteperPixel-2; j++)
+                                        //     printf("%lf\t", g[j]);
+                                        // printf("\n");
+                                        if (g[0]>190)
+                                            blank = false;
+                                        else
+                                            blank = false;
+                                    }
+                                }
                             }
                             if (blank) {
-                                memset(picData+i*realWidthSize, 0xff, picDataSize-i*realWidthSize);
+                                memset(picture+i*realWidthSize, 0xff, picDataSize-i*realWidthSize);
                                 break;
                             }
-                            memcpy(picData+i*realWidthSize, nowline, realWidthSize);
+                            memcpy(picture+i*realWidthSize, nowline, realWidthSize);
                             memcpy(lowerline, nowline, realWidthSize);
                         }
 
