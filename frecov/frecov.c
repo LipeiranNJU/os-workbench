eca0426001b47101384e9b8d5d43600fb9d5c638
tracer-nju <tracer@nju.edu.cn>
2020-05-27 20:05:56 +0800

    Static hostname: lpr-PC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 030b25f7d3ed4bf29e8db174e28df3cb            Boot ID: 9669e9f78d3b427e9c6cc16f941c0dd4   Operating System: Deepin 20 Beta             Kernel: Linux 5.3.0-3-amd64       Architecture: x86-64  20:05:56 up  2:13,  1 user,  load average: 2.41, 2.31, 1.81

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -254,7 +254,7 @@ int main (int argc, char* argv[]) {
 
                         pfdpic = fopen(abspath, "a+");
                         struct BMPInfoHeader* picInfo = (struct BMPInfoHeader*)(picStart+1);
-                        fwrite(picInfo, 1, picStart->bfSize-spicStart->bfOffBits, pfdpic);
+                        fwrite(picInfo, 1, picStart->bfSize-picStart->bfOffBits, pfdpic);
                         // assert(picStart->bfOffBits%4 == 0);
                         // assert(picInfo->biSize+sizeof(*picStart) == picStart->bfSize - picStart->bfOffBits);
                         fclose(pfdpic);
