8573f6bdade8c7f797ddfb8befeda87a0a676494
tracer-nju <tracer@nju.edu.cn>
2020-05-28 01:51:21 +0800

    Static hostname: lpr-PC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 030b25f7d3ed4bf29e8db174e28df3cb            Boot ID: 5f44185b852143ea9cea7f6212c937b5   Operating System: Deepin 20 Beta             Kernel: Linux 5.3.0-3-amd64       Architecture: x86-64  01:51:21 up 29 min,  1 user,  load average: 8.87, 9.33, 7.23

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -440,14 +440,15 @@ double* sobelY(uint8_t* lowerline, uint8_t* nowline, uint8_t* higherline, int pi
     sum = r = g = b = 0;
     double* sobel = malloc(sizeof(double)*(pixels-2));
     for (int i = 0; i < pixels; i++) {
-        r = higherline[i*3+0]+higherline[(i+1)*3+0]-lowerline[(i-1)*3+0]-2*lowerline[i*3+0]-lowerline[(i+1)*3+0];
-        g = higherline[(i-1)*3+1]+2*higherline[i*3+1]+higherline[(i+1)*3+1]-lowerline[(i-1)*3+1]-2*lowerline[i*3+1]-lowerline[(i+1)*3+1];
-        b = higherline[(i-1)*3+2]+2*higherline[i*3+2]+higherline[(i+1)*3+2]-lowerline[(i-1)*3+2]-2*lowerline[i*3+2]-lowerline[(i+1)*3+2];
-        sum += sqrt(pow(r,2)+pow(g,2)+pow(b,2));
+        r = nowline[i*3+0]-lowerline[i*3+0];
+        g = nowline[i*3+1]-lowerline[i*3+1];
+        b = nowline[i*3+2]-lowerline[i*3+2];
+        sum += pow(r,2)+pow(g,2)+pow(b,2);
         sobel[i-1] = sqrt(pow(r,2)+pow(g,2)+pow(b,2));
     }
     double* mean = malloc(sizeof(double));
-    *mean = sum/(pixels-2);
-    qsort(sobel, pixels-2, sizeof(double), comp);
+    *mean = sqrt(sum)/pixels;
+    free(sobel);
+    // qsort(sobel, pixels-2, sizeof(double), comp);
     return mean;
 }
