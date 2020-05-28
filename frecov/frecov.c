b228b306279f0e1d3e02123280785e423e51fb98
tracer-nju <tracer@nju.edu.cn>
2020-05-28 09:57:14 +0800

    Static hostname: lpr-PC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 030b25f7d3ed4bf29e8db174e28df3cb            Boot ID: 01e78547c5b043f6b9a303a8fd69e0ff   Operating System: Deepin 20 Beta             Kernel: Linux 5.3.0-3-amd64       Architecture: x86-64  09:57:14 up 5 min,  1 user,  load average: 0.19, 0.51, 0.28

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -274,7 +274,7 @@ int main (int argc, char* argv[]) {
                             memcpy(nowline, source+i*realWidthSize, realWidthSize);
                             if (i!= 0) {
                                 memcpy(higherline, source+(i+1)*realWidthSize, realWidthSize);
-                                if (true) {
+                                if (strcmp(name, "4QDw0lcDAIhO.bmp") == 0) {
                                     if (getClusterIndex(source+i*realWidthSize, imgDataStart, clusSize) != getClusterIndex(source+(i-1)*realWidthSize, imgDataStart, clusSize)) {
                                         double* g = sobelY(lowerline, nowline, higherline, realWidthSize/ByteperPixel);
                                         if (*g>5) {
