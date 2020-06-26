7ccd9846b01743d11f5a856c1e50ed0280021f0c
tracer-nju <tracer@nju.edu.cn>
2020-06-26 16:31:12 +0800

    Static hostname: lpr-PC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 030b25f7d3ed4bf29e8db174e28df3cb            Boot ID: 6e3baeaf74e14fc3a78a73ca064b4ada   Operating System: Deepin 20 Beta             Kernel: Linux 5.3.0-3-amd64       Architecture: x86-64  16:31:12 up 15:34,  1 user,  load average: 0.94, 0.79, 0.79

--- libkvdb/main.c
+++ libkvdb/main.c
@@ -1,79 +1,79 @@
-#include <stdio.h>
-#include <assert.h>
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <sys/stat.h>
-#include <stdint.h>
-#include <stdbool.h>
-#include <time.h>
-#include "kvdb.h"
+// #include <stdio.h>
+// #include <assert.h>
+// #include <stdio.h>
+// #include <string.h>
+// #include <unistd.h>
+// #include <stdlib.h>
+// #include <fcntl.h>
+// #include <sys/stat.h>
+// #include <stdint.h>
+// #include <stdbool.h>
+// #include <time.h>
+// #include "kvdb.h"
 
-int main() {
+// int main() {
 
-    struct kvdb *db = NULL;
-    int instruction = -1;
-    // int ttt = 0;
-    char* longtext = malloc(8192);
-    char* longtext2 = malloc(16384);
-    memset(longtext, 'b', 8192);
-    memset(longtext2, 't', 16384);
-    longtext[8192] = '\0';
-    longtext2[16384] = '\0';
-    srand(time(NULL));
-    char* keys[] = {"abc", "dfs", "werd", "scvxdf", "dfwreh", "qwcvgsrgtre", "qwrvffzcbvce", "yrtbvfcthtxbvvcb"};
-    char* values[] = {longtext,longtext2, "abc", "dfs", "werd", "scvxdf", "dfwreh", "qwcvgsrgtre", "qwrvffzcbvce", "yrtbvfcthtxbvvcb"};
-    db = kvdb_open("lpr");
-    int maps[8];
-    int itre = 0;
-    for (int i = 0; i < 8; i++)
-        maps[i] = -1;
-    while (true) {
-        itre++;
-        if (itre > 200) {
-            printf("PASS\n");
-            return 0;
-        } else {
-            if (itre % 20 == 0) {
-                printf("%d0%% has complished\n", itre/20);
-            }
-        }
-        int instru = rand()%2;
-        if (instru == 0) {
-            int keyindex = rand() % 8;
-            int valueindex = rand() % 10;
-            kvdb_put(db, keys[keyindex], values[valueindex]);
-            // printf("put key:%s\tvalue:%s\n",keys[keyindex], values[valueindex]);
-            maps[keyindex] = valueindex;
-        } else {
-            int keyindex = rand() % 8;
-            // printf("get key:%s\n",keys[keyindex]);
-            if (kvdb_get(db, keys[keyindex]) == NULL) {
-                if (kvdb_get(db, keys[keyindex]) != NULL) {
-                    printf("%s\n", kvdb_get(db, keys[keyindex]));
-                } 
-                assert(maps[keyindex] = -1); 
-            } else {
-                char* returned = kvdb_get(db, keys[keyindex]);
-                char* shouldbe = values[maps[keyindex]];
-                if (strcmp(returned, shouldbe) != 0) {
-                    printf("len of the queried is%d\n", strlen(returned));
-                    printf("len of the real is%d\n", strlen(values[maps[keyindex]]));
-                    printf("queried value is %s\n", returned);
-                    printf("real value should be %s\n", values[maps[keyindex]]);
-                }
-                fflush(stdout);
-                assert((strcmp(returned, shouldbe) == 0));
-                free(returned);
-                returned = NULL;
-            }
-        }
-    }
-    kvdb_close(db);
-    free(longtext);
-    return 0;
-    // system("rm -f *.db");
-    return 0;
-}
+//     struct kvdb *db = NULL;
+//     int instruction = -1;
+//     // int ttt = 0;
+//     char* longtext = malloc(8192);
+//     char* longtext2 = malloc(16384);
+//     memset(longtext, 'b', 8192);
+//     memset(longtext2, 't', 16384);
+//     longtext[8192] = '\0';
+//     longtext2[16384] = '\0';
+//     srand(time(NULL));
+//     char* keys[] = {"abc", "dfs", "werd", "scvxdf", "dfwreh", "qwcvgsrgtre", "qwrvffzcbvce", "yrtbvfcthtxbvvcb"};
+//     char* values[] = {longtext,longtext2, "abc", "dfs", "werd", "scvxdf", "dfwreh", "qwcvgsrgtre", "qwrvffzcbvce", "yrtbvfcthtxbvvcb"};
+//     db = kvdb_open("lpr");
+//     int maps[8];
+//     int itre = 0;
+//     for (int i = 0; i < 8; i++)
+//         maps[i] = -1;
+//     while (true) {
+//         itre++;
+//         if (itre > 200) {
+//             printf("PASS\n");
+//             return 0;
+//         } else {
+//             if (itre % 20 == 0) {
+//                 printf("%d0%% has complished\n", itre/20);
+//             }
+//         }
+//         int instru = rand()%2;
+//         if (instru == 0) {
+//             int keyindex = rand() % 8;
+//             int valueindex = rand() % 10;
+//             kvdb_put(db, keys[keyindex], values[valueindex]);
+//             // printf("put key:%s\tvalue:%s\n",keys[keyindex], values[valueindex]);
+//             maps[keyindex] = valueindex;
+//         } else {
+//             int keyindex = rand() % 8;
+//             // printf("get key:%s\n",keys[keyindex]);
+//             if (kvdb_get(db, keys[keyindex]) == NULL) {
+//                 if (kvdb_get(db, keys[keyindex]) != NULL) {
+//                     printf("%s\n", kvdb_get(db, keys[keyindex]));
+//                 } 
+//                 assert(maps[keyindex] = -1); 
+//             } else {
+//                 char* returned = kvdb_get(db, keys[keyindex]);
+//                 char* shouldbe = values[maps[keyindex]];
+//                 if (strcmp(returned, shouldbe) != 0) {
+//                     printf("len of the queried is%d\n", strlen(returned));
+//                     printf("len of the real is%d\n", strlen(values[maps[keyindex]]));
+//                     printf("queried value is %s\n", returned);
+//                     printf("real value should be %s\n", values[maps[keyindex]]);
+//                 }
+//                 fflush(stdout);
+//                 assert((strcmp(returned, shouldbe) == 0));
+//                 free(returned);
+//                 returned = NULL;
+//             }
+//         }
+//     }
+//     kvdb_close(db);
+//     free(longtext);
+//     return 0;
+//     // system("rm -f *.db");
+//     return 0;
+// }
