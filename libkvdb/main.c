8272292e8870cbc996e09fc3480d07da6ab50e1e
tracer-nju <tracer@nju.edu.cn>
2020-06-26 17:49:55 +0800

    Static hostname: lpr-PC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 030b25f7d3ed4bf29e8db174e28df3cb            Boot ID: 6e3baeaf74e14fc3a78a73ca064b4ada   Operating System: Deepin 20 Beta             Kernel: Linux 5.3.0-3-amd64       Architecture: x86-64  17:49:55 up 16:53,  1 user,  load average: 2.46, 1.19, 1.17

--- libkvdb/main.c
+++ libkvdb/main.c
@@ -14,7 +14,7 @@
 // #define MB (1024 * KB)
 // #define JOURNALSIZE 10
 // #define KEYSIZE (1 + 128 * B + 9 + 9 + 9 + 1)
-// #define KEYITEMS (20)
+// #define KEYITEMS (512)
 // #define KEYAREASIZE (KEYSIZE * KEYITEMS)
 
 
@@ -75,16 +75,16 @@
 //     struct record* keybuffer = malloc(sizeof(char)*KEYSIZE);
 //     sprintf(keybuffer->clusnum, "%07x", clusnum);
 //     sprintf(db->database[keyindex_i].clusnum, "%07x", clusnum);
-//     keybuffer->valid = '1';
+//     keybuffer->valid = 1;
 //     memcpy(keybuffer->keysize, keysize, 9);
 //     memcpy(keybuffer->valuesize, valuesize, 9);
 //     memcpy(keybuffer->KEY, keyname, keySizeNum);
 
 
 //     if (valueSizeNum > 4*KB) {
-//         keybuffer->isLong = '1';
+//         keybuffer->isLong = 1;
 //     } else {
-//         keybuffer->isLong = '0';
+//         keybuffer->isLong = 0;
 //     }
 //     lseek(db->fd, JOURNALSIZE+keyindex_i*KEYSIZE, SEEK_SET);
 //     write(db->fd, keybuffer, KEYSIZE);
@@ -113,18 +113,17 @@
 
 //     if (fileSize == 0) {
 //         char* zeros = malloc(JOURNALSIZE);
-//         memset(zeros, '0', JOURNALSIZE);
+//         memset(zeros, 0, JOURNALSIZE);
 //         write(pkvdb->fd, zeros, JOURNALSIZE);
 //         free(zeros);
 //         zeros = NULL;
 
-//         goto_keyarea(pkvdb);
 //         zeros = malloc(sizeof(char)*KEYAREASIZE);
-//         memset(zeros, '0', KEYAREASIZE);
+//         memset(zeros, 0, KEYAREASIZE);
 //         write(pkvdb->fd, zeros, KEYAREASIZE);
+
 //         fsync(pkvdb->fd);
-//         free(zeros);
-//         zeros = NULL;
+
 //     }
 
 
@@ -146,7 +145,7 @@
 //     load_database(db);
 //     int i;
 //     int valuelength = strlen(value);
-//     for (i = 0; db->database[i].valid != '0'; i++) {
+//     for (i = 0; db->database[i].valid != 0; i++) {
 
 
 //         if (strncmp(db->database[i].KEY, key, strlen(key)) == 0) {
@@ -155,7 +154,7 @@
 //     }
 
 
-//     if (db->database[i].valid == '0') {
+//     if (db->database[i].valid == 0) {
 //         setkeyondisk(db, key, value, i);
 //         lseek(db->fd, 0, SEEK_END);
 //         if (valuelength <= 4*KB) {
@@ -176,9 +175,7 @@
 //             valuebuffer = NULL;
 //         }
 //     } else {
-//         if (db->database[i].isLong == '0' && valuelength >= 4*KB) {
-//             assert(db->database[i].isLong == '0');
-//             assert(db->database[i].valid == '1');
+//         if (db->database[i].isLong == 0 && valuelength >= 4*KB) {
 //             setkeyondisk(db, key, value, i);
 //             int end = lseek(db->fd, 0, SEEK_END);
 //             int clus = strtol(db->database[i].clusnum, NULL, 16);
@@ -198,7 +195,7 @@
 //             sprintf(valuesize, "%07x", valueSizeNum);
 //             lseek(db->fd, JOURNALSIZE+i*KEYSIZE, SEEK_SET);
 //             struct record* keybuffer = malloc(sizeof(char)*KEYSIZE);
-//             keybuffer->valid = '1';
+//             keybuffer->valid = 1;
 //             memcpy(keybuffer->keysize, keysize, 9);
 //             memcpy(keybuffer->valuesize, valuesize, 9);
 //             memcpy(keybuffer->KEY, key, keySizeNum);
@@ -208,14 +205,14 @@
 //             int clusindex = strtol(db->database[i].clusnum, NULL, 16);
 //             int isLong;
 //             char* valuebuff;
-//             if (db->database[i].isLong == '0') {
+//             if (db->database[i].isLong == 0) {
 //                 isLong = 0;
-//                 keybuffer->isLong = '0';
+//                 keybuffer->isLong = 0;
 //                 valuebuff = malloc(4*KB);
 //                 memset(valuebuff, 'R', 4*KB);
-//             } else if (db->database[i].isLong == '1') {
+//             } else if (db->database[i].isLong == 1) {
 //                 isLong = 1;
-//                 keybuffer->isLong = '1';
+//                 keybuffer->isLong = 1;
 //                 valuebuff = malloc(16*MB);
 //                 memset(valuebuff, 'Y', 16*MB);
 //             } else{
@@ -255,7 +252,7 @@
 //     int clusNum = -1;
 //     int valueSize = -1;
 //     char* returned = NULL;
-//     for (int i = 0; db->database[i].valid != '0'; i++) {
+//     for (int i = 0; db->database[i].valid != 0; i++) {
 //         if (strncmp(db->database[i].KEY, key, strtol(db->database[i].keysize, NULL, 16)) == 0) {
 //             clusNum = strtol(db->database[i].clusnum, NULL, 16);
 //             valueSize = strtol(db->database[i].valuesize, NULL, 16);
