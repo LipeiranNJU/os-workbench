843e5ce40746ace7219c1c471f2d73d14e38b674
tracer-nju <tracer@nju.edu.cn>
2020-06-26 17:51:36 +0800

    Static hostname: lpr-PC          Icon name: computer-laptop            Chassis: laptop         Machine ID: 030b25f7d3ed4bf29e8db174e28df3cb            Boot ID: 6e3baeaf74e14fc3a78a73ca064b4ada   Operating System: Deepin 20 Beta             Kernel: Linux 5.3.0-3-amd64       Architecture: x86-64  17:51:36 up 16:55,  1 user,  load average: 1.18, 1.14, 1.16

--- libkvdb/kvdb.c
+++ libkvdb/kvdb.c
@@ -14,7 +14,7 @@
 #define MB (1024 * KB)
 #define JOURNALSIZE 10
 #define KEYSIZE (1 + 128 * B + 9 + 9 + 9 + 1)
-#define KEYITEMS (64)
+#define KEYITEMS (32)
 #define KEYAREASIZE (KEYSIZE * KEYITEMS)
 
 
